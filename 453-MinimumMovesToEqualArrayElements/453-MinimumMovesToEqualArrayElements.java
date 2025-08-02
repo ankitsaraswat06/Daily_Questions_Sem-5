e4faf7f663ba9cc5190474e94f3e107846ebe1c5
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-02 18:56:34 +0530

    Create 453-MinimumMovesToEqualArrayElements

---
+++ 453-MinimumMovesToEqualArrayElements/453-MinimumMovesToEqualArrayElements.java
@@ -0,0 +1,19 @@
+// Last updated: 02/08/2025, 18:56:34
+class Solution {
+    public int minMoves(int[] nums) {
+        int moves = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]<min) {
+                min = nums[i];
+            }
+        }
+
+        for(int i=0; i<nums.length; i++) {
+            moves+=nums[i]-min;
+        }
+
+        return moves;
+        
+    }
+}
