b0d96cabac721839d3a7b683b1f2418d6291f770
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-02 18:56:05 +0530

    Create 647-PalindromicSubstrings

---
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -0,0 +1,26 @@
+// Last updated: 02/08/2025, 18:56:05
+class Solution {
+    public int countSubstrings(String s) {
+        int odd = 0;
+        // odd length
+        for(int axis = 0; axis<s.length(); axis++) {
+            for(int orbit = 0; axis-orbit>=0 && axis+orbit <s.length(); orbit++) {
+                if(s.charAt(axis-orbit) != s.charAt(axis+orbit)) {
+                    break;
+                }  
+                odd++; 
+            }
+        }
+        // even length
+        int even =0;
+        for(double axis = 0.5; axis<s.length(); axis++) {
+            for(double orbit = 0.5; axis-orbit>=0 && axis+orbit <s.length(); orbit++) {
+                if(s.charAt((int)(axis-orbit)) != s.charAt((int)(axis+orbit))) {
+                    break;
+                }  
+                even++; 
+            }
+        }
+        return odd+even;
+    }
+}
