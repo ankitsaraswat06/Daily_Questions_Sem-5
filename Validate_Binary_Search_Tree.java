cb2ea2bb6dd58f014c9f6a6a9acec702399cbf6d
Ankit Saraswat <ankitsaraswat@Ankits-MacBook-Air.local>
2025-08-02 09:52:59 +0530

    third question

---
+++ Validate_Binary_Search_Tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class BstPair {
+        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
+        boolean isBst = true;
+    }
+    public BstPair isValid(TreeNode root) {
+        if(root==null) {
+            return new BstPair();
+        }
+        BstPair lbp = isValid(root.left);
+        BstPair rbp = isValid(root.right);
+        BstPair sbp = new BstPair();
+        sbp.max = Math.max(root.val, Math.max(lbp.max, rbp.max));
+        sbp.min = Math.min(root.val, Math.min(lbp.min, rbp.min));
+        sbp.isBst = lbp.isBst && rbp.isBst && lbp.max < root.val && rbp.min > root.val;
+        return sbp;
+
+    }
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root).isBst;
+
+    }
+}
