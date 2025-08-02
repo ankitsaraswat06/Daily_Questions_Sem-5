b2a1ecbc8c083717da5461b817a2bc314537849b
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-02 18:53:09 +0530

    Create 2377-CheckIfNumberHasEqualDigitCountAndDigitValue

---
+++ 2377-CheckIfNumberHasEqualDigitCountAndDigitValue/2377-CheckIfNumberHasEqualDigitCountAndDigitValue.java
@@ -0,0 +1,14 @@
+// Last updated: 02/08/2025, 18:53:09
+class Solution {
+    public boolean digitCount(String num) {
+        int[] freq = new int[11];
+        for(int i=0; i<num.length(); i++) {
+            freq[num.charAt(i)-'0']++;
+        }
+        for(int i=0; i<num.length(); i++) {
+            if((num.charAt(i)-'0') != freq[i]) return false;
+        }
+        return true;
+        
+    }
+}
