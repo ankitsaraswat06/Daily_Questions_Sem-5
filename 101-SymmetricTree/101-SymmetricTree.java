ea99525c806b05afdb3e92ec23ccd42f9cfec945
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-02 18:58:39 +0530

    Create 101-SymmetricTree

---
+++ 101-SymmetricTree/101-SymmetricTree.java
@@ -0,0 +1,30 @@
+// Last updated: 02/08/2025, 18:58:39
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean check(TreeNode root1, TreeNode root2) {
+        if(root1==null && root2==null) return true;
+        if(root1==null || root2 == null) return false;
+        if(root1.val!=root2.val) return false;
+        boolean left = check(root1.left, root2.right);
+        boolean right = check(root1.right, root2.left);
+        return left && right;
+
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left, root.right);
+    }
+}
