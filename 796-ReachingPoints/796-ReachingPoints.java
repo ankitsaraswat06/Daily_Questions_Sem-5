09ae5fb75d6e8a07064a0d048d897a5de3f96493
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-02 18:55:43 +0530

    Create 796-ReachingPoints

---
+++ 796-ReachingPoints/796-ReachingPoints.java
@@ -0,0 +1,12 @@
+// Last updated: 02/08/2025, 18:55:43
+class Solution {
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while(tx>sx && ty>sy) {
+            if(tx>ty) tx%=ty;
+            else ty%=tx;
+        }
+        if(tx==sx && ty >= sy) return (ty-sy)%tx==0;
+        else if(ty==sy && tx>=sx) return (tx-sx)%ty==0;
+        else return false;
+    }
+}
