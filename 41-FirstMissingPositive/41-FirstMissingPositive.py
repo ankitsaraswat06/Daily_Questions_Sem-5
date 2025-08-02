05182634d67b1933d18c13891e0c7a370a5bea49
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-02 18:59:30 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.py
@@ -0,0 +1,16 @@
+# Last updated: 02/08/2025, 18:59:29
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = [n for n in nums if n > 0]
+        nums.sort()
+
+        target = 1
+        for n in nums:
+            if n == target:
+                target += 1
+            elif n > target:
+                return target
+        
+        return target
+
+        
