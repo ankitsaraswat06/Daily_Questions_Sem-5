1ca4a8b6baa854fe36f186c05b19356abbb3243f
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-02 18:52:39 +0530

    Create 2655-RearrangeArrayToMaximizePrefixScore

---
+++ 2655-RearrangeArrayToMaximizePrefixScore/2655-RearrangeArrayToMaximizePrefixScore.java
@@ -0,0 +1,17 @@
+// Last updated: 02/08/2025, 18:52:39
+class Solution {
+    public int maxScore(int[] nums) {
+        Arrays.sort(nums);
+        long sum = 0;
+        int ans = 0;
+        for(int i=nums.length-1; i>=0; i--) {
+            sum+=nums[i];
+            if(sum<=0) {
+                break;
+            } else {
+                ans++;
+            }
+        }
+        return ans;  
+    }
+}
