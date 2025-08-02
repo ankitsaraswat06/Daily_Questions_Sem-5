dea320d7cdbc33464ab1269f53991bf5c66f597a
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-02 18:55:09 +0530

    Create 1026-StringWithoutAaaOrBbb

---
+++ 1026-StringWithoutAaaOrBbb/1026-StringWithoutAaaOrBbb.java
@@ -0,0 +1,28 @@
+// Last updated: 02/08/2025, 18:55:08
+class Solution {
+    public String strWithout3a3b(int a, int b) {
+        StringBuilder sb=new StringBuilder();
+        while(a>0||b>0){
+            if(a>b && b!=0){
+                sb.append("aab");
+                a=a-2;
+                b--;
+            }
+            else if(b>a && a!=0){
+                sb.append("bba");
+                b=b-2;
+                a--;
+            }
+            else if(a>0){
+                sb.append("a");
+                a--;
+            }
+            else if(b>0){
+                sb.append("b");
+                b--;
+            }
+        }
+        return sb.toString();
+        
+    }
+}
