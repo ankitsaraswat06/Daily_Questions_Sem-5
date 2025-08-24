a4867698cad55836bf742091392c04338f68dc8d
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-24 11:13:33 +0530

    Create 2432-NumberOfZeroFilledSubarrays

---
+++ 2432-NumberOfZeroFilledSubarrays/2432-NumberOfZeroFilledSubarrays.java
@@ -0,0 +1,18 @@
+// Last updated: 24/08/2025, 11:13:33
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long ans =0;
+        long cont = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]==0) {
+                cont++;
+            } else {
+                ans = ans + (cont * (cont+1))/2;
+                cont = 0;
+            }
+        }
+        ans = ans + (cont * (cont+1))/2;
+        return ans;
+
+    }
+}
