4c951037c099dd423c8d55e9b3d3ebf688e2b332
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-30 06:54:29 +0530

    File updated at 30/08/2025, 06:54:29

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,11 +1,14 @@
-// Last updated: 30/08/2025, 06:51:36
+// Last updated: 30/08/2025, 06:54:29
 class Solution {
     
     public int climbStairs(int n) {
-        int []dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2; i<dp.length; i++) dp[i] = dp[i-1] + dp[i-2];
-        return dp[n];
+        int prev2 = 1;
+        int prev = 1;
+        for(int i=2; i<=n; i++) {
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 }
