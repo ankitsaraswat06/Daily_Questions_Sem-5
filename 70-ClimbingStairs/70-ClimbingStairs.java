1e23091bb23d05ff9d3cfccaafba846e4bb9e0a1
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-08-30 06:51:36 +0530

    File updated at 30/08/2025, 06:51:36

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,14 +1,11 @@
-// Last updated: 30/08/2025, 06:48:55
+// Last updated: 30/08/2025, 06:51:36
 class Solution {
-    public int ways(int n, int[] dp) {
-        if(n<0) return 0;
-        if(n<=2) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = ways(n-1,dp) + ways(n-2,dp);  
-    }
+    
     public int climbStairs(int n) {
         int []dp = new int[n+1];
-        for(int i=0; i<dp.length; i++) dp[i] = -1;
-        return ways(n,dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<dp.length; i++) dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 }
