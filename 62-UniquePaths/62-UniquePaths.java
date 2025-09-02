1a7d8d0327504f1229d559c4ba55b040bf8410c2
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-02 15:40:32 +0530

    File updated at 02/09/2025, 15:40:32

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,18 +1,18 @@
-// Last updated: 02/09/2025, 15:15:35
+// Last updated: 02/09/2025, 15:40:32
 class Solution {
-    public int rec(int m, int n, int row, int col, int[][] dp) {
-        if(row>=m || col >= n) return 0;
-        if(row==m-1 && col == n-1) return 1;
-        if(dp[row][col] !=-1) return dp[row][col];
-        int a = rec(m, n, row+1, col, dp);
-        int b = rec(m, n, row, col+1, dp);
-        return dp[row][col] = a+b;
-        
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i=0; i<m; i++) 
-        for(int j=0; j<n; j++) dp[i][j] = -1;
-        return rec(m, n, 0, 0, dp);
+        dp[0][0] = 1;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i==0 && j==0) continue;
+                int up = 0;
+                if(i>0)up =  dp[i-1][j];
+                int left =0;
+                if(j>0)left =  dp[i][j-1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
