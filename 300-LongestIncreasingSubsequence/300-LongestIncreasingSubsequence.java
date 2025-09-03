9801e6e04261fa17773354081e8c9e75fc2e0a38
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-03 12:35:41 +0530

    File updated at 03/09/2025, 12:35:40

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,18 +1,16 @@
-// Last updated: 03/09/2025, 12:31:19
+// Last updated: 03/09/2025, 12:35:40
 class Solution {
     public int lengthOfLIS(int[] arr) {
         int dp[] = new int[arr.length];
         Arrays.fill(dp, 1);
-        int max = 1;
         for(int i=1; i<arr.length; i++) {
             for(int j=i-1; j>=0; j--) {
                 if(arr[i] > arr[j]) {
                     dp[i] = Math.max(dp[i], dp[j]+1);
-                    max = Math.max(dp[i], max);
                 }
             }
         }
-        return max;
+        return Arrays.stream(dp).max().getAsInt();
 
     }
 }
