b83313873fafc5b2a427c13207b9d74304b22cbd
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-04 12:09:28 +0530

    File updated at 04/09/2025, 12:09:27

--- 518-CoinChangeII/518-CoinChangeII.java
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -1,25 +1,21 @@
-// Last updated: 04/09/2025, 11:34:22
+// Last updated: 04/09/2025, 12:09:27
 class Solution {
-    public int rec(int[] coin, int amount, int i, int[][] dp) {
-        if(amount ==0) return 1;
-        if(i==coin.length) {
-            return 0;
+    public int change(int amount, int[] coin) {
+        int[][] dp = new int[coin.length + 1][amount+1];
+        for(int i=0; i<dp.length; i++) {//row
+            dp[i][0] = 1;
         }
-        if(dp[amount][i]!=-1) return dp[amount][i];
-        int inc = 0, exc = 0;
-        if(amount>=coin[i]) {
-            inc = rec(coin, amount-coin[i], i, dp);
-        }
-        exc = rec(coin, amount, i+1, dp);
-        return dp[amount][i] = inc + exc;
-    }
-    public int change(int amount, int[] coins) {
-        int[][] dp = new int[amount+1][coins.length];
-        for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[0].length; j++) {
-                dp[i][j] = -1;
+        for(int i=1; i<dp.length; i++) {
+            for (int am =1; am<dp[0].length; am++) {
+                int inc = 0, exc = 0;
+                if(am>=coin[i-1]) {
+                    inc  = dp[i][am- coin[i-1]];
+                }
+                exc = dp[i-1][am];
+                dp[i][am] = inc+exc;
             }
         }
-        return rec(coins, amount, 0, dp);
+
+        return dp[dp.length-1][dp[0].length-1];
     }
 }
