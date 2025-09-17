c34bf1666a9835bac47b4578010b6dc5a57e52d7
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-17 12:05:55 +0530

    File updated at 17/09/2025, 12:05:54

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,16 +1,32 @@
-// Last updated: 03/09/2025, 12:35:40
+// Last updated: 17/09/2025, 12:05:54
 class Solution {
-    public int lengthOfLIS(int[] arr) {
-        int dp[] = new int[arr.length];
-        Arrays.fill(dp, 1);
-        for(int i=1; i<arr.length; i++) {
-            for(int j=i-1; j>=0; j--) {
-                if(arr[i] > arr[j]) {
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                }
+    public int binarySearch(int[] dp, int si, int ei, int item) {
+        int idx = 0;
+        while(si<=ei) {
+            int mid = (si+ei)/2;
+            if(dp[mid]>=item) {
+                idx = mid;
+                ei = mid -1;
+            } else {
+                si = mid+1;
             }
         }
-        return Arrays.stream(dp).max().getAsInt();
+        return idx;
+    }
 
+    public int lengthOfLIS(int[] arr) {
+        int[] dp = new int[arr.length];
+        int len = 1;
+        dp[0] = arr[0];
+        for(int i=1; i<dp.length; i++) {
+            if(arr[i] > dp[len-1]) {
+                dp[len] = arr[i];
+                len++;
+            } else {
+                int idx = binarySearch(dp, 0, len-1, arr[i]);
+                dp[idx] = arr[i];
+            }
+        }
+        return len;
     }
 }
