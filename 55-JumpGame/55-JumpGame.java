606becda3b4f94cdf38b759bc28543a2356cdbb8
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-17 22:13:34 +0530

    File updated at 17/09/2025, 22:13:34

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,15 +1,13 @@
-// Last updated: 17/09/2025, 22:12:32
+// Last updated: 17/09/2025, 22:13:34
 class Solution {
     public boolean rec(int [] nums, int idx, Boolean[] dp) {
         if(idx>=nums.length) return false;
         if(idx==nums.length-1) return true;
         if(dp[idx]!=null) return dp[idx];
-        boolean pos = false;
         for(int i=1; i<=nums[idx]; i++) {
-            pos = pos || rec(nums, idx+i, dp);
-            if(pos==true) break;
+            if(rec(nums, idx+i, dp)) return dp[idx] = true;
         }
-        return dp[idx] = pos;
+        return dp[idx] = false;
     }
     public boolean canJump(int[] nums) {
         Boolean dp[] = new Boolean[nums.length];
