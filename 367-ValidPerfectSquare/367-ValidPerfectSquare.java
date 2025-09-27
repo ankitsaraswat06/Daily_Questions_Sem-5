1413d6bc7924951bb96e5b7a3aad5440995e6965
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-27 21:44:44 +0530

    File updated at 27/09/2025, 21:44:44

--- 367-ValidPerfectSquare/367-ValidPerfectSquare.java
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.java
@@ -1,27 +1,22 @@
-// Last updated: 02/08/2025, 18:57:05
+// Last updated: 27/09/2025, 21:44:44
 class Solution {
     public boolean isPerfectSquare(int num) {
-        long i = 0;
-        if(num == 0 || num == 1)
-        {
-            return true;
+        // binary search lagake finding 
+        if(num==1) return true;
+        int lo = 1;
+        int hi = num;
+        while(lo<=hi) {
+            int mid = lo + (hi-lo)/2;
+            long sq = (long) mid*mid;
+            if(sq==num) {
+                return true;
+            } else if(sq<num) {
+                lo = mid+1;
+            } else {
+                hi = mid-1;
+            }
         }
-		while(i <= num/2)
-		{
-			if(i * i < num)
-			{
-                i++;
-				continue;
-			}
-			else if(i * i == num)
-			{
-				return true;
-			}
-			else
-			{
-				break;
-			}
-		}
-        return false;        
+        return false;
+        
     }
 }
