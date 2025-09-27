64e9f479d1ae9d2d7dadf03fce88c4d5fd9d78fa
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-27 15:14:23 +0530

    Create 875-KokoEatingBananas

---
+++ 875-KokoEatingBananas/875-KokoEatingBananas.java
@@ -0,0 +1,29 @@
+// Last updated: 27/09/2025, 15:14:23
+class Solution {
+    public boolean isPossible(int [] piles, int h, int speed) {
+        int hour = 0;
+        for(int i=0; i<piles.length; i++) {
+            int a  = (int) (Math.ceil((double)piles[i]/speed));
+            hour = hour + a;
+            if(hour>h) return false;
+        }
+        
+        return true;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int low = 1;
+        int high = piles[piles.length-1];
+        int ans = 0;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if(isPossible(piles, h, mid)) {
+                ans = mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
