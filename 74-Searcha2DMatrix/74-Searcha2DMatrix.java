491185418ce4d93a375071d9de9200991a7187be
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-29 13:15:49 +0530

    File updated at 29/09/2025, 13:15:48

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.java
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.java
@@ -1,21 +1,12 @@
-// Last updated: 30/08/2025, 04:46:24
+// Last updated: 29/09/2025, 13:15:48
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       int sR = 0, eR = matrix.length-1;
-       int sC = 0, eC = matrix[0].length-1;
-       while(sR<=eR && sC <= eC) {
-        int midR = sR + (eR-sR)/2;
-        int midC = sC + (eC-sC)/2;
-        if(matrix[midR][midC] == target) return true;
-        else if(matrix[midR][midC] < target && matrix[midR][matrix[0].length-1] < target) {
-            sR = midR+1;
-        } else if(matrix[midR][midC] > target && matrix[midR][0] > target) {
-            eR = midR-1;
-        } else if(matrix[midR][midC] > target) {
-            eC = midC-1;
-        } else {
-            sC = midC+1;
-        }
+       int row = 0;
+       int col = matrix[0].length-1;
+       while(row<matrix.length && col >= 0) {
+        if(matrix[row][col] == target) return true;
+        if(matrix[row][col] > target) col--;
+        else row++;
        }
        return false;
     }
