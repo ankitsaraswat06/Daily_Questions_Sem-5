a3272f0607b0550b86e65bed7b76894869f4bd68
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-29 12:11:55 +0530

    File updated at 29/09/2025, 12:11:54

--- 832-FlippinganImage/832-FlippinganImage.java
+++ 832-FlippinganImage/832-FlippinganImage.java
@@ -1,23 +1,22 @@
-// Last updated: 29/09/2025, 12:08:16
+// Last updated: 29/09/2025, 12:11:54
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         for(int i=0; i<image.length; i++) {
             int j= 0;
             int k = image[0].length-1;
-            while(j<k) {
-                int temp = image[i][j];
-                image[i][j] = image[i][k];
+            while(j<=k) {
+                if(j==k) {
+                    image[i][j]^=1;
+                    break;
+                }
+                int temp = image[i][j] ^ 1;
+                image[i][j] = image[i][k] ^ 1;
                 image[i][k] = temp;
                 j++;
                 k--;
             }
         }
-        for(int i=0; i<image.length; i++) {
-            for(int j=0; j<image[0].length; j++) {
-                if(image[i][j]==0) image[i][j] = 1;
-                else image[i][j] = 0;
-            }
-        }
+        
         return image;
     }
 }
