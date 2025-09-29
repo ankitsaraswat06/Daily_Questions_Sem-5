9a42347998b6c12b75773915df72da4122424926
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-29 13:37:18 +0530

    File updated at 29/09/2025, 13:37:18

--- 48-RotateImage/48-RotateImage.java
+++ 48-RotateImage/48-RotateImage.java
@@ -1,24 +1,32 @@
-// Last updated: 02/08/2025, 18:59:17
+// Last updated: 29/09/2025, 13:37:18
 class Solution {
     public void rotate(int[][] matrix) {
-        for(int row=0; row<matrix.length; row++) {
-            for(int col = row+1; col<matrix[0].length; col++) {
-                int temp = matrix[row][col];
-                matrix[row][col] = matrix[col][row];
-                matrix[col][row] = temp;
+        // reverse columms
+        for(int j=0; j<matrix[0].length; j++) {
+            int i = 0;
+            int k = matrix.length-1; 
+            while(i<k) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[k][j];
+                matrix[k][j] = temp;
+                i++;
+                k--;
             }
         }
 
-        for(int col = 0; col<matrix[0].length/2; col++) {
-            for(int row = 0; row<matrix.length; row++) {
-                int temp = matrix[row][col];
-                matrix[row][col] = matrix[row][matrix[0].length-1-col];
-                matrix[row][matrix[0].length-1-col] = temp;
+        // transpose matrix
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(i<j) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
             }
-
         }
-
-
         
+
+
+
     }
 }
