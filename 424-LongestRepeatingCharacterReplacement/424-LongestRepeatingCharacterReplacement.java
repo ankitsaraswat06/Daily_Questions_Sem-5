c899c3e740dfc32c1207a0ad2da44d911b47fdd8
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-30 21:54:12 +0530

    File updated at 30/09/2025, 21:54:12

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
@@ -1,26 +1,22 @@
-// Last updated: 02/08/2025, 18:56:46
+// Last updated: 30/09/2025, 21:54:12
 class Solution {
     public int characterReplacement(String s, int k) {
-        int [] freq = new int[26];
-        int si=0, ei=0, maxFreq=0, maxLength=0;
+        int freq[] = new int[26];
+        int si=0;
+        int ei=0;
+        int maxFreq = 0;
+        int ans =0;
         while(ei<s.length()) {
-            // grow window
             freq[s.charAt(ei)-'A']++;
-            //max freq of any character upto ei
             maxFreq = Math.max(maxFreq, freq[s.charAt(ei)-'A']);
-            // shrink window while window is invalid(windowlength-maxFreq > k)
             int windowSize = ei-si+1;
-            if(windowSize-maxFreq > k && si<=ei) {
+            if(windowSize-maxFreq > k) {
                 freq[s.charAt(si)-'A']--;
                 si++;
             }
-
-            // update answer 
-            maxLength = Math.max(maxLength, ei-si+1);
-            ei++;       
+            ans = Math.max(ans, ei-si+1);
+            ei++;
         }
-
-        return maxLength;
-        
+        return ans;
     }
 }
