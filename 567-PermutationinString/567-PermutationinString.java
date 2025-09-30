d8305414abc74bcb66ad98e01c6023755aa2b8ef
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-30 21:29:00 +0530

    File updated at 30/09/2025, 21:29:00

--- 567-PermutationinString/567-PermutationinString.java
+++ 567-PermutationinString/567-PermutationinString.java
@@ -1,25 +1,29 @@
-// Last updated: 30/09/2025, 11:46:33
+// Last updated: 30/09/2025, 21:29:00
 class Solution {
+    public static boolean isSame(int[] arr1, int[] arr2) {
+        for(int i=0; i<26; i++) {
+            if(arr1[i]!=arr2[i]) return false;
+        }
+        return true;
+    }
     public boolean checkInclusion(String s1, String s2) {
         int[] freq1 = new int[26];
         for(int i=0; i<s1.length(); i++) {
             freq1[s1.charAt(i)-'a']++;
         }
-        int freq2[] = new int[26];
-        int si = 0;
-        int count = 0;
-        for(int ei =0; ei<s2.length(); ei++) {
-            freq2[s2.charAt(ei)-'a']++;
-            count++;
-            while(count>s1.length()) {
+        int ei=0;
+        int si =0;
+        int[] freq2 = new int[26];
+        while(ei<s2.length()) {
+            while(ei-si+1>s1.length()) {
+                if(isSame(freq1, freq2)) return true;
                 freq2[s2.charAt(si)-'a']--;
-                count--;
                 si++;
             }
-            if(Arrays.equals(freq1, freq2)) {
-                return true;
-            }
-        } 
+            freq2[s2.charAt(ei)-'a']++;
+            ei++;
+        }
+        if(isSame(freq1, freq2)) return true;
         return false;
     }
 }
