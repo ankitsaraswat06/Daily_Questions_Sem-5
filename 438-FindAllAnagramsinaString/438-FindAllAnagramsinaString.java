e2c5776d4d078f1547cdf003114aa48a7b94e29d
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-09-30 11:52:06 +0530

    File updated at 30/09/2025, 11:52:05

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
@@ -1,32 +1,30 @@
-// Last updated: 30/09/2025, 11:30:23
+// Last updated: 30/09/2025, 11:52:05
 class Solution {
-    public boolean isSame(int[] freq1, int[] freq2) {
-        for(int i=0; i<26; i++) {
-            if(freq1[i]!=freq2[i]) return false;
+    public boolean isZero(int[] freq) {
+        for(int k: freq) {
+            if(k!=0) return false;
         }
         return true;
     }
     public List<Integer> findAnagrams(String s, String p) {
-        int [] pFreq = new int[26];
+        int[] freq = new int[26];
         for(int i=0; i<p.length(); i++) {
-            pFreq[p.charAt(i)-'a']++;
+            freq[p.charAt(i)-'a']++;
         }
-        int si = 0;
-        int ei = 0;
-        int [] sFreq = new int[26];
         List<Integer> ans = new ArrayList<>();
+        int ei=0;
+        int si = 0;
         while(ei<s.length()) {
-            while(ei-si+1 > p.length()) {
-                if(isSame(pFreq, sFreq)) {
+            freq[s.charAt(ei)-'a']--;
+            while(ei-si+1 == p.length() && si<s.length()) {
+                if(isZero(freq)) {
                     ans.add(si);
                 }
-                sFreq[s.charAt(si)-'a']--;
+                freq[s.charAt(si)-'a']++;
                 si++;
             }
-            sFreq[s.charAt(ei)-'a']++;
             ei++;
         }
-        if(isSame(pFreq, sFreq)) ans.add(s.length()-p.length());
         return ans;
     }
 }
