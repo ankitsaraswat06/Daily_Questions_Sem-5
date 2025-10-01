844f4d856a8aaa73e25f2bfa9b1264fcde390007
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-10-01 14:38:42 +0530

    File updated at 01/10/2025, 14:38:42

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,10 +1,11 @@
-// Last updated: 01/10/2025, 14:36:56
+// Last updated: 01/10/2025, 14:38:42
 class Solution {
     public int jump(int[] nums) {
         int near = 0, far = 0, jump = 0;
         int n = nums.length;
+        int farthest = 0;
         while(far<n-1) {
-            int farthest = 0;
+            
             for(int i=near; i<=far; i++) {
                 farthest = Math.max(farthest, i + nums[i]);
             }
