5ee5cc2af0e1e261556400fcd04863831daefdff
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-10-01 15:04:23 +0530

    File updated at 01/10/2025, 15:04:23

--- 135-Candy/135-Candy.java
+++ 135-Candy/135-Candy.java
@@ -1,25 +1,20 @@
-// Last updated: 02/08/2025, 18:58:23
+// Last updated: 01/10/2025, 15:04:23
 class Solution {
     public int candy(int[] ratings) {
-        int sum =0;
-        int ans[] = new int[ratings.length];
-        for(int i=0; i<ratings.length; i++) {
-            ans[i] = 1;
-        }
+        int[] arr = new int[ratings.length];
+        Arrays.fill(arr,1);
         for(int i=1; i<ratings.length; i++) {
-            if(ratings[i-1] < ratings[i]) {
-                ans[i] = ans[i-1] + 1;
+            if(ratings[i] > ratings[i-1]) {
+                arr[i] = arr[i-1]+1;
             }
         }
         for(int i=ratings.length-2; i>=0; i--) {
-            if(ratings[i] > ratings[i+1]) {
-                ans[i] = Math.max(ans[i], ans[i+1]+1);
+            if(ratings[i]>ratings[i+1]) {
+                arr[i] = Math.max(arr[i], arr[i+1]+1);
             }
         }
-        for(int i=0; i<ans.length; i++) {
-            sum+=ans[i];
-        }
+        int sum =0;
+        for(int i: arr) sum+=i;
         return sum;
-        
     }
 }
