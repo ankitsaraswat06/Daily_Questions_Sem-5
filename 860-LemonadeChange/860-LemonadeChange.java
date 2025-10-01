7409dae600eb50cc7467fe3000933ff5efa5a96c
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-10-01 21:28:45 +0530

    File updated at 01/10/2025, 21:28:44

--- 860-LemonadeChange/860-LemonadeChange.java
+++ 860-LemonadeChange/860-LemonadeChange.java
@@ -1,27 +1,25 @@
-// Last updated: 01/10/2025, 21:25:29
+// Last updated: 01/10/2025, 21:28:44
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int[] freq = new int[3];
+        int five = 0;
+        int ten = 0;
         for(int i=0; i<bills.length; i++) {
-            if(bills[i]==5) freq[0]++;
+            if(bills[i]==5) five++;
             if(bills[i]==10) {
-                if(freq[0]==0) return false;
-                freq[0]--;
-                freq[1]++;
+                if(five>0) {
+                    five--;
+                    ten++;
+                } else return false;
             }
             if(bills[i]==20) {
-                if(freq[1]>=1 && freq[0]>=1) {
-                    freq[1]--;
-                    freq[0]--;
-                    freq[2]++;
-                } else if(freq[0]>=3) {
-                    freq[0]-=3;
-                    freq[2]++;
-                } else {
-                    return false;
-                }
+                if(five>0 && ten>0) {
+                    ten--;
+                    five--;
+                } else if(five>=3) {
+                    five-=3;
+                } else return false;
             }
-        }  
+        }
         return true;
     }
 }
