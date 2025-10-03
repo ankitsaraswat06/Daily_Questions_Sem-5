3bf6bdcebec5bb1c64ae213f8f249e9941bbc571
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-10-03 20:38:16 +0530

    File updated at 03/10/2025, 20:38:15

--- 77-Combinations/77-Combinations.java
+++ 77-Combinations/77-Combinations.java
@@ -1,19 +1,22 @@
-// Last updated: 02/08/2025, 18:58:53
+// Last updated: 03/10/2025, 20:38:15
 class Solution {
-    public void print(int n, int k, int curr, ArrayList<Integer> ll, List<List<Integer>> ans) {
-        if(k==0) {
-            ans.add(new ArrayList<>(ll));
-            return;
+    public static void rec(int n, int k, List<Integer> ll, List<List<Integer>> ans) {
+        if(n==0) {
+            if(ll.size()==k) ans.add(new ArrayList<>(ll));
+            return; 
         }
-        for(int i = curr; i<=n; i++) {
-            ll.add(i);
-            print(n, k-1, i+1, ll, ans);
-            ll.remove(ll.size()-1);
-        }
+        // pick
+        ll.add(n);
+        rec(n-1, k, ll, ans);
+        ll.remove(ll.size()-1);
+        //not pick
+        rec(n-1, k, ll, ans);
     }
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        print(n, k, 1, new ArrayList<Integer>(), ans);
+        List<Integer> ll = new ArrayList<>();
+        rec(n, k, ll, ans);
+
         return ans;
     }
 }
