fc139f0ad5564907a57e53ee6018b689a3866c64
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-10-04 14:40:13 +0530

    File updated at 04/10/2025, 14:40:12

--- 37-SudokuSolver/37-SudokuSolver.java
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -1,32 +1,59 @@
-// Last updated: 02/08/2025, 18:59:27
+// Last updated: 04/10/2025, 14:40:12
 class Solution {
-    public boolean isVal(char[][] board, int row, int col, char c) {
-        for(int i=0; i<9; i++) {
-            if(board[i][col]==c) return false;
-            if(board[row][i] == c) return false;
-            if(board[3*(row/3) + i/3][3*(col/3) + i%3]==c) return false;
-
-        }
-        return true;
+    public void solveSudoku(char[][] board) {
+        fill_Suduko(board, 0, 0);
     }
-    public boolean solve(char[][] board) {
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board.length; j++) {
-                if(board[i][j]=='.') {
-                    for(char c='1'; c<='9'; c++) {
-                        if(isVal(board, i, j, c)){
-                            board[i][j] = c;
-                            if(solve(board)==true) return true;
-                            else board[i][j] = '.';
-                        }
+
+    public boolean fill_Suduko(char[][] board, int row ,int col){
+        if(col ==9){
+            row++;
+            col =0;
+        }
+        if(row == 9){
+            return true;
+        }
+        if(board[row][col] != '.'){
+            return fill_Suduko(board, row ,col+1);
+        }else{
+            for(int val =1 ;val<=9;val++){
+                if(isitsafe(board, row, col, val)){
+                    board[row][col] = (char)(val+'0');
+                    boolean ans = fill_Suduko(board, row, col+1);
+                    if(ans){
+                        return true;
                     }
+                    board[row][col] = '.';
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isitsafe(char[][] board, int row, int col, int val){
+        //row
+        char ch = (char)(val+'0');
+        for(int i =0 ;i<board.length ;i++){
+            if(board[i][col] == ch){
+                return false;
+            }
+        }
+
+        for(int i =0 ;i<board[0].length ;i++){
+            if(board[row][i] == ch){
+                return false;
+            }
+        }
+
+        int r = row - row%3;
+        int c = col - col%3;
+        for(int i= r ;i< r+3 ;i++){
+            for(int j = c ;j< c+3 ;j++){
+                if(board[i][j] == ch){
                     return false;
                 }
             }
         }
         return true;
-    }
-    public void solveSudoku(char[][] board) {
-        solve(board); 
+
     }
 }
