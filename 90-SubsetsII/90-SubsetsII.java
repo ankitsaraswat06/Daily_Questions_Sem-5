134454e1892206caf7ce83163a65181236244a6b
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-10-04 11:47:26 +0530

    File updated at 04/10/2025, 11:47:25

--- 90-SubsetsII/90-SubsetsII.java
+++ 90-SubsetsII/90-SubsetsII.java
@@ -1,20 +1,21 @@
-// Last updated: 04/10/2025, 11:46:20
+// Last updated: 04/10/2025, 11:47:25
 class Solution {
-    public void subSet(int[] nums, int idx, ArrayList<Integer> ll, List<List<Integer>> ans) {
-        ans.add(new ArrayList<>(ll));
+    public void rec(int[] nums, int idx, List<Integer> ll, List<List<Integer>> ans) {
 
+        ans.add(new ArrayList<>(ll));
         for(int i=idx; i<nums.length; i++) {
             if(i>idx && nums[i]==nums[i-1]) continue;
             ll.add(nums[i]);
-            subSet(nums, i+1, ll, ans);
+            rec(nums, i+1, ll, ans);
             ll.remove(ll.size()-1);
         }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        ArrayList<Integer> ll = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        subSet(nums, 0, ll, ans); 
+        List<Integer> ll = new ArrayList<>();
+        rec(nums, 0, ll, ans);
         return ans;
+        
     }
 }
