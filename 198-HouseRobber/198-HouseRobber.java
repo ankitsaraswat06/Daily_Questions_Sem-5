54c7c866d45805c4ff2666123245e95d5d0bebb3
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-10-06 11:42:12 +0530

    File updated at 06/10/2025, 11:42:11

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,10 +1,11 @@
-// Last updated: 06/10/2025, 11:39:42
+// Last updated: 06/10/2025, 11:42:11
 class Solution {
 
     public int rob(int[] arr) {
        int prev2 =0;
         int prev = arr[0];
         for(int i=1; i<arr.length; i++) {
+            if(arr[i]<0) continue;
             int pick = arr[i] + prev2;
             int nonpick = prev;
             int curr = Math.max(pick, nonpick);
