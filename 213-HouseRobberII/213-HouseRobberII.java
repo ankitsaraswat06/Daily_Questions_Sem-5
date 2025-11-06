3d173e6a3d26e21303d945c5694bd9f4e97bc18d
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-11-06 14:58:01 +0530

    File updated at 06/11/2025, 14:58:00

--- 213-HouseRobberII/213-HouseRobberII.java
+++ 213-HouseRobberII/213-HouseRobberII.java
@@ -1,29 +1,21 @@
-// Last updated: 06/10/2025, 11:48:12
+// Last updated: 06/11/2025, 14:58:00
 class Solution {
+    public int findRob(int[] arr, int si, int ei) {
+        int prev2 = 0;
+        int prev = arr[si];
+        for(int i=si+1; i<=ei; i++) {
+            int take = arr[i] + prev2;
+            int nontake = prev + 0;
+            int curr = Math.max(take, nontake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
     public int rob(int[] arr) {
         if(arr.length==1) return arr[0];
-        int prev2 =0;
-        int prev = arr[0];
-        for(int i=1; i<arr.length-1; i++) {
-            int pick = arr[i] + prev2;
-            int nonpick = prev;
-            int curr = Math.max(pick, nonpick);
-            prev2 = prev;
-            prev = curr;
-
-        }
-        int ans1 = prev;
-        prev2 =0;
-        prev = arr[1];
-        for(int i=2; i<arr.length; i++) {
-            int pick = arr[i] + prev2;
-            int nonpick = prev;
-            int curr = Math.max(pick, nonpick);
-            prev2 = prev;
-            prev = curr;
-
-        }
-        int ans2 = prev;
-        return Math.max(ans1, ans2);
+        int a = findRob(arr, 0, arr.length-2);
+        int b = findRob(arr, 1, arr.length-1);
+        return Math.max(a,b);
     }
 }
