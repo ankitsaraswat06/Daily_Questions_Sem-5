2873ff6357977d8f1918a49233da30af58eed480
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-11-08 15:17:32 +0530

    File updated at 08/11/2025, 15:17:31

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,18 +1,30 @@
-// Last updated: 06/10/2025, 10:18:12
+// Last updated: 08/11/2025, 15:17:31
 class Solution {
+    public int f(int i, int j, int[][] dp) {
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = f(i-1, j, dp);
+        int left = f(i, j-1, dp);
+        return dp[i][j] = up+left;
+    }
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        dp[0][0] = 1;
+        // for(int arr[]: dp) Arrays.fill(arr, -1);
+        // return f(m-1, n-1, dp);
+        // Tabulation Approach
+        dp[0][0] =1;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(i==0 && j==0) continue;
                 int up = 0;
-                if(i>0)up =  dp[i-1][j];
-                int left =0;
-                if(j>0)left =  dp[i][j-1];
-                dp[i][j] = up + left;
+                if(i>=1) up = dp[i-1][j];
+                int left = 0;
+                if(j>=1) left = dp[i][j-1];
+                dp[i][j]=up+left;
             }
         }
         return dp[m-1][n-1];
+
     }
 }
