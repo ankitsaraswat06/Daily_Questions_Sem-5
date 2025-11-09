9322aca01fefc92a9667cf382c90ad8e9824eb69
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-11-09 10:44:33 +0530

    File updated at 09/11/2025, 10:44:33

--- 64-MinimumPathSum/64-MinimumPathSum.java
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -1,4 +1,4 @@
-// Last updated: 09/11/2025, 10:32:14
+// Last updated: 09/11/2025, 10:44:33
 class Solution {
     public int f(int i, int j, int[][] grid, int[][] dp) {
         if(i==0 && j==0) return grid[i][j];
@@ -12,7 +12,20 @@ class Solution {
         int m = grid.length; 
         int n= grid[0].length;
         int[][] dp = new int[m][n];
-        for(int []arr: dp) Arrays.fill(arr,-1);
-        return f(m-1, n-1, grid, dp);
+        // for(int []arr: dp) Arrays.fill(arr,-1);
+        // return f(m-1, n-1, grid, dp);
+        // Tabulation Approach
+        dp[0][0] = grid[0][0];
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i==0 && j==0) continue;
+                int up=Integer.MAX_VALUE;
+                if(i>0) up= dp[i-1][j];
+                int left = Integer.MAX_VALUE;
+                if(j>0) left = dp[i][j-1];
+                dp[i][j] = Math.min(up, left) + grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
