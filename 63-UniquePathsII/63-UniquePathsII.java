78095c5dd18ae32a0b125e4977e00730b3aba964
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-11-09 10:20:36 +0530

    File updated at 09/11/2025, 10:20:35

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,4 +1,4 @@
-// Last updated: 09/11/2025, 10:14:38
+// Last updated: 09/11/2025, 10:20:35
 class Solution {
     public int f(int i, int j, int [][] grid, int[][] dp) {
         if(i<0 || j<0) return 0;
@@ -14,8 +14,23 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        for(int[] arr: dp) Arrays.fill(arr, -1);
-        return f(m-1, n-1, grid, dp);
+        // for(int[] arr: dp) Arrays.fill(arr, -1);
+        // return f(m-1, n-1, grid, dp);
+        // Tabulation Approach
+
+        if(grid[0][0]!=1) dp[0][0] = 1;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i==0 && j==0) continue;
+                if(grid[i][j] == 1) continue;
+                int up = 0;
+                if(i>0)up =  dp[i-1][j];
+                int left =0;
+                if(j>0) left =  dp[i][j-1];
+                dp[i][j] = up+left;
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 }
