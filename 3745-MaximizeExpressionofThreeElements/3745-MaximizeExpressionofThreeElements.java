7f7235f11317636b27079342ad8ec545f5e1c857
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-11-16 11:45:15 +0530

    File updated at 16/11/2025, 11:45:15

--- 3745-MaximizeExpressionofThreeElements/3745-MaximizeExpressionofThreeElements.java
+++ 3745-MaximizeExpressionofThreeElements/3745-MaximizeExpressionofThreeElements.java
@@ -1,17 +1,22 @@
-// Last updated: 16/11/2025, 11:41:38
+// Last updated: 16/11/2025, 11:45:15
 class Solution {
     public int maximizeExpressionOfThree(int[] nums) {
-        int max  = Integer.MIN_VALUE;
-        for(int i=0; i<nums.length; i++) {
-            for(int j=i+1; j<nums.length; j++) {
-                for(int k=j+1; k<nums.length; k++) {
-                    max = Math.max(nums[i]+nums[j] - nums[k], max);
-                    max = Math.max(nums[j]+nums[k]-nums[i], max);
-                    max = Math.max(nums[i]+nums[k]-nums[j], max);
-                }
+        int max = Integer.MIN_VALUE;
+        int sMax = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int el: nums) {
+            if(el>max) {
+                sMax = max;
+                max = el;
+            } else if(el>sMax) {
+                sMax = el;
+            } 
+
+            if(el<min) {
+                min =el;
             }
         }
-        return max;
+        return max+sMax-min;
         
     }
 }
