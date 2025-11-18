031b27bcb8de13ea12ad53a32898d6886c24157b
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-11-18 21:51:15 +0530

    File updated at 18/11/2025, 21:51:15

--- 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.java
+++ 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.java
@@ -1,17 +1,11 @@
-// Last updated: 18/11/2025, 21:49:54
+// Last updated: 18/11/2025, 21:51:15
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
-        int prev = 0;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i]==1) {
-                prev = i;
-                break;
-            }
-        }
+        int prev = -1;
         for(int i=prev+1; i<nums.length; i++) {
             if(nums[i]==1) {
                 int dis = i-prev-1;
-                if(dis<k) return false;
+                if(dis<k && prev!=-1) return false;
                 prev = i;
             }
         }
