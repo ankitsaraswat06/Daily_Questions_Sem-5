185d488bfb7db9bb1201d93701335a8c309c15d4
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-11-22 18:15:23 +0530

    File updated at 22/11/2025, 18:15:23

--- 3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree/3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
+++ 3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree/3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
@@ -1,9 +1,9 @@
-// Last updated: 22/11/2025, 18:13:34
+// Last updated: 22/11/2025, 18:15:23
 class Solution {
     public int minimumOperations(int[] nums) {
         int ans = 0;
         for(int i=0; i<nums.length; i++) {
-            ans += Math.min(nums[i]%3, 3 - nums[i]%3);
+           if(nums[i]%3!=0) ans++;
         }
         return ans;
         
