2c328a7a0a2c49db6e903a110bb7e4fd35ea3e4d
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-11-23 20:56:44 +0530

    File updated at 23/11/2025, 20:56:43

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.java
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -1,18 +1,8 @@
-// Last updated: 23/11/2025, 20:55:39
+// Last updated: 23/11/2025, 20:56:43
 class Solution {
-    public void emptyRow(int[][] arr, int row) {
-        for(int col=0; col<arr[0].length; col++) {
-            arr[row][col] = 0;
-        }
-    }
-    public void emptyCol(int[][] arr, int col) {
-        for(int row=0; row<arr.length; row++) {
-            arr[row][col] = 0;
-        }
-    }
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer> rows = new HashSet<>();
-        HashSet<Integer> cols = new HashSet<>();
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
         for(int i=0; i<matrix.length; i++) {
             for(int j=0; j<matrix[0].length; j++) {
                 if(matrix[i][j]==0) {
@@ -21,12 +11,15 @@ class Solution {
                 }
             }
         }
-        for(int row: rows) {
-            emptyRow(matrix, row);
+
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
         }
-        for(int col: cols) {
-            emptyCol(matrix, col);
-        }
+        
         
     }
 }
