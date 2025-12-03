c06aef13b1b76f6d11fc5fbaa967e1c10b2ad74d
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-03 15:51:08 +0530

    File updated at 03/12/2025, 15:51:08

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,17 +1,25 @@
-// Last updated: 02/08/2025, 18:59:04
-class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
-        // System.out.print(intervals);
-        List<int[]> ans = new ArrayList<>();
-        ans.add(intervals[0]);
-        for(int i=1; i<intervals.length; i++) {
-            if(ans.get(ans.size()-1)[1] >= intervals[i][0]) {
-                ans.get(ans.size()-1)[1] = Math.max(ans.get(ans.size()-1)[1], intervals[i][1]);
-            } else {
-                ans.add(intervals[i]);
-            }
-        }
-        return ans.toArray(new int[ans.size()][]);
-    }
-}
+// Last updated: 03/12/2025, 15:51:08
+1class Solution {
+2    public int[][] merge(int[][] intervals) {
+3        Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
+4        List<int[]> ans = new ArrayList<>();
+5        ans.add(intervals[0]);
+6        for(int i=1; i<intervals.length; i++) {
+7            int prevEnd = ans.get(ans.size()-1)[1];
+8            int newStart = intervals[i][0];
+9            if(prevEnd>=newStart) {
+10                ans.get(ans.size()-1)[1] = Math.max(prevEnd, intervals[i][1]);
+11            } else {
+12                ans.add(intervals[i]);
+13            }
+14        }
+15
+16        int[][] f = new int[ans.size()][2];
+17        for(int i=0; i<ans.size(); i++) {
+18            f[i][0] = ans.get(i)[0];
+19            f[i][1]  = ans.get(i)[1];
+20        }
+21        return f;
+22        
+23    }
+24}
