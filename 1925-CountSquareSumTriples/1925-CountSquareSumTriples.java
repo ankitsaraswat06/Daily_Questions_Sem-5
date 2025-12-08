4e6e5c510e6c44e1ec9518656c924523f1ccfa6f
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-08 18:43:23 +0530

    File updated at 08/12/2025, 18:43:23

--- 1925-CountSquareSumTriples/1925-CountSquareSumTriples.java
+++ 1925-CountSquareSumTriples/1925-CountSquareSumTriples.java
@@ -1,15 +1,14 @@
-// Last updated: 08/12/2025, 18:40:15
+// Last updated: 08/12/2025, 18:43:23
 1class Solution {
 2    public int countTriples(int n) {
 3        int c=0;
 4        for(int i=1; i<=n; i++) {
 5            for(int j=1; j<=n; j++) {
-6                for(int k=1; k<=n; k++) {
-7                    if(i*i+j*j==k*k) c++;
-8                }
-9            }
-10        }
-11        return c;
-12        
-13    }
-14}
+6                int root = (int) Math.sqrt(i*i+j*j);
+7                if(root*root == i*i+j*j && root <= n) c++; 
+8            }
+9        }
+10        return c;
+11        
+12    }
+13}
