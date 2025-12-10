b5e895e0f3d8de2d8f2bce0c49b7d386cd5fa2c0
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-10 21:17:30 +0530

    File updated at 10/12/2025, 21:17:30

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,4 +1,4 @@
-// Last updated: 10/12/2025, 21:15:59
+// Last updated: 10/12/2025, 21:17:30
 1class Solution {
 2    public int trap(int[] height) {
 3        int n = height.length;
@@ -10,11 +10,10 @@
 9        int total = 0;
 10        int leftMax = Integer.MIN_VALUE;
 11        for(int i=0; i<n; i++) {
-12            if(height[i]<rightMax[i]) leftMax = Math.max(leftMax, height[i]);
-13            else leftMax = rightMax[i];
-14            total += Math.min(leftMax, rightMax[i])-height[i];
-15            
-16        }
-17        return total;
-18    }
-19}
+12            leftMax = Math.max(leftMax, height[i]);
+13            total += Math.min(leftMax, rightMax[i])-height[i];
+14            
+15        }
+16        return total;
+17    }
+18}
