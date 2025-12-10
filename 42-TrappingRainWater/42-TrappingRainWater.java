e9f465b77d92e7e6703a9841b6659c8c19bcb5c1
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-10 21:15:59 +0530

    File updated at 10/12/2025, 21:15:59

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,21 +1,20 @@
-// Last updated: 10/12/2025, 21:07:23
+// Last updated: 10/12/2025, 21:15:59
 1class Solution {
 2    public int trap(int[] height) {
 3        int n = height.length;
-4        int[] leftMax = new int[n];
-5        leftMax[0] = height[0];
-6        for(int i=1; i<n; i++) {
-7            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+4        int[] rightMax = new int[n];
+5        rightMax[n-1] = height[n-1];
+6        for(int i=n-2; i>=0; i--) {
+7            rightMax[i] = Math.max(rightMax[i+1], height[i]);
 8        }
-9        int[] rightMax = new int[n];
-10        rightMax[n-1] = height[n-1];
-11        for(int i=n-2; i>=0; i--) {
-12            rightMax[i] = Math.max(rightMax[i+1], height[i]);
-13        }
-14        int total = 0;
-15        for(int i=0; i<n; i++) {
-16            total += Math.min(leftMax[i], rightMax[i])-height[i];
-17        }
-18        return total;
-19    }
-20}
+9        int total = 0;
+10        int leftMax = Integer.MIN_VALUE;
+11        for(int i=0; i<n; i++) {
+12            if(height[i]<rightMax[i]) leftMax = Math.max(leftMax, height[i]);
+13            else leftMax = rightMax[i];
+14            total += Math.min(leftMax, rightMax[i])-height[i];
+15            
+16        }
+17        return total;
+18    }
+19}
