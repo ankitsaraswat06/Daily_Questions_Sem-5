1c3316df8a881743703fd215d78d39a0f3e111b9
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-10 21:59:53 +0530

    File updated at 10/12/2025, 21:59:52

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,19 +1,26 @@
-// Last updated: 10/12/2025, 21:17:30
+// Last updated: 10/12/2025, 21:59:52
 1class Solution {
-2    public int trap(int[] height) {
-3        int n = height.length;
-4        int[] rightMax = new int[n];
-5        rightMax[n-1] = height[n-1];
-6        for(int i=n-2; i>=0; i--) {
-7            rightMax[i] = Math.max(rightMax[i+1], height[i]);
-8        }
-9        int total = 0;
-10        int leftMax = Integer.MIN_VALUE;
-11        for(int i=0; i<n; i++) {
-12            leftMax = Math.max(leftMax, height[i]);
-13            total += Math.min(leftMax, rightMax[i])-height[i];
-14            
-15        }
-16        return total;
-17    }
-18}
+2    public int trap(int[] arr) {
+3        int n = arr.length;
+4        int total=0, lMax=0, rMax=0, l=0, r=n-1;
+5        while(l<r) {
+6            if(arr[l]<=arr[r]) {
+7                if(lMax>arr[l]) {
+8                    total+=lMax-arr[l];
+9                } else {
+10                    lMax = arr[l];
+11                }
+12                l=l+1;
+13            } else {
+14                if(rMax>arr[r]) {
+15                    total+=rMax-arr[r];
+16                } else {
+17                    rMax = arr[r];
+18                }
+19                r = r-1;
+20            }
+21        }
+22        return total;
+23        
+24    }
+25}
