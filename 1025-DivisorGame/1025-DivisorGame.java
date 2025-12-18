f153280384fd4de3c7adcffe6aa7d62a93f7fd7d
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-18 20:29:33 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/1025-DivisorGame.java
@@ -0,0 +1,8 @@
+// Last updated: 18/12/2025, 20:29:33
+1class Solution {
+2    public boolean divisorGame(int n) {
+3        if(n%2==0) return true;
+4        else return false;
+5        
+6    }
+7}
