dedaa55666e9df3780f6c4c3479c9bb031773744
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-18 14:56:38 +0530

    File updated at 18/12/2025, 14:56:38

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,18 +1,16 @@
-// Last updated: 02/08/2025, 18:56:16
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int sum = 0, count =0;
-        for(int si =0; si<nums.length; si++) {
-            sum = 0;
-            for(int ei = si; ei<nums.length; ei++) {
-                sum+=nums[ei];
-                if(sum==k) {
-                    count++;
-                }
-            }
-            
-        }
-        return count;
-        
-    }
-}
+// Last updated: 18/12/2025, 14:56:38
+1class Solution {
+2    public int subarraySum(int[] nums, int k) {
+3        int preSum = 0;
+4        int ans =0;
+5        Map<Integer, Integer> map =new HashMap<>();
+6        for(int i=0; i<nums.length; i++) {
+7            preSum+=nums[i];
+8            if(preSum==k) ans++;
+9            if(map.containsKey(preSum-k)) ans+=map.get(preSum-k);
+10            map.put(preSum, map.getOrDefault(preSum, 0)+1);
+11        }
+12        return ans;
+13        
+14    }
+15}
