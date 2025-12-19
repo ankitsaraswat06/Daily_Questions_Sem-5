16405842c48633b52cbf8c9a7956db7d18924e89
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-19 10:36:38 +0530

    File updated at 19/12/2025, 10:36:38

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,15 +1,15 @@
-// Last updated: 15/11/2025, 13:42:41
-class Solution {
-    public int maxArea(int[] height) {
-        int i = 0;
-        int j = height.length-1;
-        int max = 0;
-        while(i<j) {
-            max = Math.max(Math.min(height[i], height[j]) * (j-i), max);
-            if(height[i] < height[j]) i++;
-            else j--;
-
-        }
-        return max;
-    }
-}
+// Last updated: 19/12/2025, 10:36:38
+1class Solution {
+2    public int maxArea(int[] height) {
+3        int i=0;
+4        int j = height.length-1;
+5        int ans = 0;
+6        while(i<j) {
+7            ans = Math.max(Math.min(height[i], height[j]) * (j-i), ans);
+8            if(height[i]<height[j]) i++;
+9            else j--;
+10        }
+11        
+12        return ans;
+13    }
+14}
