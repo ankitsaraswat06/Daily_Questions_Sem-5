2b8f83d0bae6f09ff43174addbafec35f4cfd408
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-19 10:22:39 +0530

    File updated at 19/12/2025, 10:22:38

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,16 +1,17 @@
-// Last updated: 02/08/2025, 19:00:08
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int [] ans = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++) {
-            int required = target - nums[i];
-            if(map.containsKey(required)) {
-                ans[0] = map.get(required);
-                ans[1] = i;
-            }
-            map.put(nums[i], i);
-        }
-        return ans;
-    }
-}
+// Last updated: 19/12/2025, 10:22:38
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        HashMap<Integer, Integer> map = new HashMap<>();
+4        int[] ans = new int[2];
+5        for(int i=0; i<nums.length; i++) {
+6            int compliment = target - nums[i];
+7            if(map.containsKey(compliment)) {
+8                ans[0] = map.get(compliment);
+9                ans[1] = i;
+10                break;
+11            }
+12            map.put(nums[i], i);
+13        }
+14        return ans;
+15    }
+16}
