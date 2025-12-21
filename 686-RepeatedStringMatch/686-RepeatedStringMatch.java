313ea13ec156ab40519f0c7950f8c39f95ce4e4f
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-21 08:44:35 +0530

    File updated at 21/12/2025, 08:44:35

--- 686-RepeatedStringMatch/686-RepeatedStringMatch.java
+++ 686-RepeatedStringMatch/686-RepeatedStringMatch.java
@@ -1,16 +1,16 @@
-// Last updated: 02/08/2025, 18:56:02
-class Solution {
-    public int repeatedStringMatch(String a, String b) {
-        int c = 0;
-        StringBuilder n = new StringBuilder();
-        while(n.length() < b.length()) {
-            n.append(a);
-            c++;
-        }
-        if(n.toString().contains(b)) return c;
-        n.append(a);
-        c++;
-        if(n.toString().contains(b)) return c;
-        return -1;
-    }
-}
+// Last updated: 21/12/2025, 08:44:35
+1class Solution {
+2    public int repeatedStringMatch(String a, String b) {
+3        int c = 0;
+4        StringBuilder n = new StringBuilder();
+5        while(n.length() < b.length()) {
+6            n.append(a);
+7            c++;
+8        }
+9        if(n.toString().contains(b)) return c;
+10        n.append(a);
+11        c++;
+12        if(n.toString().contains(b)) return c;
+13        return -1;
+14    }
+15}
