e7dd5452204d21183ef5f4f20be70f0ae2ffb9d6
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-25 17:37:12 +0530

    File updated at 25/12/2025, 17:37:12

--- 2591-DistributeMoneytoMaximumChildren/2591-DistributeMoneytoMaximumChildren.java
+++ 2591-DistributeMoneytoMaximumChildren/2591-DistributeMoneytoMaximumChildren.java
@@ -1,18 +1,19 @@
-// Last updated: 25/12/2025, 12:39:18
+// Last updated: 25/12/2025, 17:37:12
 1class Solution {
 2    public int distMoney(int money, int children) {
 3        if (money < children) return -1;
 4
-5        money -= children; 
-6        int ans = Math.min(money / 7, children);
-7        if (ans == children && money != 7 * children) return children - 1;
-8
-9        if (ans < children && money - ans * 7 == 3 && ans == children - 1)
-10            ans--;
-11
-12        return ans;
-13
-14        
+5        int canGive = money/8;
+6        if(canGive>children) return children-1;
+7
+8        while(canGive>0 && (money-canGive*8)<children-canGive) canGive--;
+9
+10        int remain = children-canGive;
+11        if((money-canGive*8)==4 && remain==1) canGive--;
+12        else if((money-canGive*8)>0 && remain==0) canGive--;
+13        return canGive;
+14
 15        
-16    }
-17}
+16        
+17    }
+18}
