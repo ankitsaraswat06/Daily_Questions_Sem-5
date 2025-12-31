a50fc6e5c8f1f0e269d4ae182812d59a733c2777
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-31 22:47:56 +0530

    File updated at 31/12/2025, 22:47:56

--- 40-CombinationSumII/40-CombinationSumII.java
+++ 40-CombinationSumII/40-CombinationSumII.java
@@ -1,22 +1,22 @@
-// Last updated: 03/10/2025, 21:22:31
-class Solution {
-    public static void cS(int idx, int[] arr, int target, ArrayList<Integer> ll, List<List<Integer>> ans ) {
-        if(target == 0) {
-            ans.add(new ArrayList<>(ll));
-            return;
-        }
-        for(int i=idx; i<arr.length; i++) {
-            if(i>idx && arr[i] == arr[i-1]) continue;
-            if(arr[i]>target) break;
-            ll.add(arr[i]);
-            cS(i+1, arr, target-arr[i], ll, ans);
-            ll.remove(ll.size()-1);
-        }
-    }
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> ans = new ArrayList<>();
-        cS(0, candidates, target, new ArrayList<Integer>(), ans);
-        return ans;
-    }
-}
+// Last updated: 31/12/2025, 22:47:56
+1class Solution {
+2    public static void cS(int idx, int[] arr, int target, ArrayList<Integer> ll, List<List<Integer>> ans ) {
+3        if(target == 0) {
+4            ans.add(new ArrayList<>(ll));
+5            return;
+6        }
+7        for(int i=idx; i<arr.length; i++) {
+8            if(i>idx && arr[i] == arr[i-1]) continue;
+9            if(arr[i]>target) break;
+10            ll.add(arr[i]);
+11            cS(i+1, arr, target-arr[i], ll, ans);
+12            ll.remove(ll.size()-1);
+13        }
+14    }
+15    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+16        Arrays.sort(candidates);
+17        List<List<Integer>> ans = new ArrayList<>();
+18        cS(0, candidates, target, new ArrayList<Integer>(), ans);
+19        return ans;
+20    }
+21}
