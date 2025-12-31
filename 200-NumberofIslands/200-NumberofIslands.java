614cd0156a20b12e733038248536efeb718ae4e4
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2025-12-31 23:37:25 +0530

    File updated at 31/12/2025, 23:37:25

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,28 +1,30 @@
-// Last updated: 29/09/2025, 20:11:57
-class Solution {
-    public void dfs(char[][] grid, int i, int j) {
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]!='1') {
-            return;
-        }
-        grid[i][j] = 'V';
-        dfs(grid, i-1, j);
-        dfs(grid, i+1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
-    }
-    public int numIslands(char[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        int islands = 0;
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<cols; j++) {
-                if(grid[i][j]=='1') {
-                    dfs(grid, i, j);
-                    islands++;
-                }
-            }
-        }
-        return islands;
-    }
-}
+// Last updated: 31/12/2025, 23:37:25
+1class Solution {
+2    public void dfs(char[][] grid, int i, int j) {
+3        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]!='1') {
+4            return;
+5        }
+6        grid[i][j] = 'V';
+7        dfs(grid, i-1, j);
+8        dfs(grid, i+1, j);
+9        dfs(grid, i, j-1);
+10        dfs(grid, i, j+1);
+11
+12        
+13    }
+14    public int numIslands(char[][] grid) {
+15        int rows = grid.length;
+16        int cols = grid[0].length;
+17
+18        int islands = 0;
+19        for(int i=0; i<rows; i++) {
+20            for(int j=0; j<cols; j++) {
+21                if(grid[i][j]=='1') {
+22                    dfs(grid, i, j);
+23                    islands++;
+24                }
+25            }
+26        }
+27        return islands;
+28    }
+29}
