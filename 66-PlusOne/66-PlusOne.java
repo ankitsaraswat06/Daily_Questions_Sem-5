e7b25d0264713e2e0fb901b8cf46b611f62254e2
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-01 10:59:04 +0530

    File updated at 01/01/2026, 10:59:04

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,16 +1,16 @@
-// Last updated: 02/08/2025, 18:59:02
-class Solution {
-    public int[] plusOne(int[] digits) {
-        for(int i=digits.length-1; i>=0; i--) {
-            digits[i] = digits[i] + 1;
-            if(digits[i] < 10) {
-                return digits;
-            } 
-            digits[i] = 0;
-        }  
-
-        int ans[] = new int[digits.length + 1];
-        ans[0] = 1;
-        return ans;
-    }
-}
+// Last updated: 01/01/2026, 10:59:04
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        for(int i=digits.length-1; i>=0; i--) {
+4            digits[i] = digits[i] + 1;
+5            if(digits[i] < 10) {
+6                return digits;
+7            } 
+8            digits[i] = 0;
+9        }  
+10
+11        int ans[] = new int[digits.length + 1];
+12        ans[0] = 1;
+13        return ans;
+14    }
+15}
