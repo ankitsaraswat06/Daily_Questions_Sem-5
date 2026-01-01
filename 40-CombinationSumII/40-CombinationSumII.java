d66ddd074e7e4db8ea576f1b57d71dd94af71dae
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-01 12:03:29 +0530

    File updated at 01/01/2026, 12:03:29

--- 40-CombinationSumII/40-CombinationSumII.java
+++ 40-CombinationSumII/40-CombinationSumII.java
@@ -1,22 +1,27 @@
-// Last updated: 31/12/2025, 22:47:56
+// Last updated: 01/01/2026, 12:03:29
 1class Solution {
-2    public static void cS(int idx, int[] arr, int target, ArrayList<Integer> ll, List<List<Integer>> ans ) {
-3        if(target == 0) {
-4            ans.add(new ArrayList<>(ll));
-5            return;
-6        }
-7        for(int i=idx; i<arr.length; i++) {
-8            if(i>idx && arr[i] == arr[i-1]) continue;
-9            if(arr[i]>target) break;
-10            ll.add(arr[i]);
-11            cS(i+1, arr, target-arr[i], ll, ans);
-12            ll.remove(ll.size()-1);
-13        }
-14    }
-15    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-16        Arrays.sort(candidates);
-17        List<List<Integer>> ans = new ArrayList<>();
-18        cS(0, candidates, target, new ArrayList<Integer>(), ans);
-19        return ans;
-20    }
-21}
+2    public void rec(int[] candidates, int target, int idx, List<List<Integer>> ans, List<Integer> ll) {
+3        
+4        if(target==0) {
+5            ans.add(new ArrayList<>(ll));
+6            return;
+7        }
+8        if(idx>=candidates.length) return;
+9        for(int i=idx; i<candidates.length; i++) {
+10            if(i>idx && candidates[i] == candidates[i-1]) continue;
+11            if(target-candidates[i]>=0) {
+12                ll.add(candidates[i]);
+13                rec(candidates, target-candidates[i], i+1, ans, ll);
+14                ll.remove(ll.size()-1);
+15            }
+16        }
+17    }
+18    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+19        List<List<Integer>> ans = new ArrayList<>();
+20        List<Integer> ll = new ArrayList<>();
+21        Arrays.sort(candidates);
+22        rec(candidates, target, 0, ans, ll);
+23        return ans;
+24        
+25    }
+26}
