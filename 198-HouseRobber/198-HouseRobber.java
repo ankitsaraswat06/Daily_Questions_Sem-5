70bd35185b3fb3d0ef9cc1d2cb3a7e6b4ba66e3c
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-03 12:48:11 +0530

    File updated at 03/01/2026, 12:48:11

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,17 +1,16 @@
-// Last updated: 06/11/2025, 14:53:06
-class Solution {
-
-    public int rob(int[] arr) {
-       int prev2 =0;
-        int prev = arr[0];
-        for(int i=1; i<arr.length; i++) {
-            int pick = arr[i] + prev2;
-            int nonpick = prev;
-            int curr = Math.max(pick, nonpick);
-            prev2 = prev;
-            prev = curr;
-
-        }
-        return prev; 
-    }
-}
+// Last updated: 03/01/2026, 12:48:11
+1class Solution {
+2    public int rec(int[] nums, int idx, int[] dp) {
+3        if(idx>=nums.length) return 0;
+4        if(dp[idx]!=-1) return dp[idx];
+5        int pick = rec(nums, idx+2, dp) + nums[idx];
+6        int nonpick = rec(nums, idx+1, dp);
+7        return dp[idx] = Math.max(pick, nonpick);
+8    }
+9    public int rob(int[] nums) {
+10        int[] dp = new int[nums.length+1];
+11        Arrays.fill(dp, -1);
+12        return rec(nums, 0, dp);
+13        
+14    }
+15}
