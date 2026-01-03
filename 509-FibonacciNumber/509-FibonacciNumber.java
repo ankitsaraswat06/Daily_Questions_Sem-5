6c171022403650412c5909e4fb11452e60f692cc
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-03 12:04:49 +0530

    File updated at 03/01/2026, 12:04:49

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,13 +1,14 @@
-// Last updated: 03/01/2026, 12:01:02
+// Last updated: 03/01/2026, 12:04:49
 1class Solution {
-2    public int rec(int n, int[] dp) {
-3        if(n==0 || n==1) return n;
-4        if(dp[n]!=-1) return dp[n];
-5        return dp[n] = rec(n-1, dp) + rec(n-2, dp);
-6    }
-7    public int fib(int n) {
-8        int dp[] = new int[n+1];
-9        Arrays.fill(dp, -1);
-10        return rec(n, dp);
-11    }
-12}
+2
+3    public int fib(int n) {
+4        if(n==0 || n==1) return n;
+5        int dp[] = new int[n+1];
+6        dp[0] = 0;
+7        dp[1] = 1;
+8        for(int i=2; i<dp.length; i++) {
+9            dp[i] = dp[i-1]+dp[i-2];
+10        }
+11        return dp[n];
+12    }
+13}
