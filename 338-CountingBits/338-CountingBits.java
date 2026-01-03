29949864dc920315fde898cad68dfbdfa7973e19
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-03 13:02:04 +0530

    File updated at 03/01/2026, 13:02:04

--- 338-CountingBits/338-CountingBits.java
+++ 338-CountingBits/338-CountingBits.java
@@ -1,15 +1,14 @@
-// Last updated: 06/10/2025, 11:27:24
-class Solution {
-    public int[] countBits(int n) {
-
-        
-        int[] dp = new int[n+1];
-        if(n==0) return dp;
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i=2; i<n+1; i++) {
-            dp[i] = dp[i/2] + i%2;
-        }
-        return dp;
-    }
-}
+// Last updated: 03/01/2026, 13:02:04
+1class Solution {
+2    public int rec(int n) {
+3        if(n==0) return 0;
+4        return rec(n/2) + n%2;
+5    }
+6    public int[] countBits(int n) {
+7        int[] ans = new int[n+1];
+8        for(int i=0; i<=n; i++) {
+9            ans[i] = rec(i);
+10        }
+11        return ans;
+12    }
+13}
