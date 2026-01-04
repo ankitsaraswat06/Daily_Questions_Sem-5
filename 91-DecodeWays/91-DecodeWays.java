b3748596c3dc5ce5cf2afef140ebc3136e095839
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-04 15:27:20 +0530

    File updated at 04/01/2026, 15:27:20

--- 91-DecodeWays/91-DecodeWays.java
+++ 91-DecodeWays/91-DecodeWays.java
@@ -1,25 +1,37 @@
-// Last updated: 04/01/2026, 15:19:54
+// Last updated: 04/01/2026, 15:27:20
 1class Solution {
-2    public int rec(String s, int idx, int[] dp) {
-3        if (idx == s.length()) return 1;
-4        if (s.charAt(idx) == '0') return 0;
-5        if (dp[idx] != -1) return dp[idx];
-6
-7        int ways = rec(s, idx + 1, dp);
-8
-9        if (idx + 1 < s.length()) {
-10            int val = Integer.parseInt(s.substring(idx, idx + 2));
-11            if (val >= 10 && val <= 26) {
-12                ways += rec(s, idx + 2, dp);
-13            }
-14        }
-15
-16        return dp[idx] = ways;
-17    }
-18
-19    public int numDecodings(String s) {
-20        int[] dp = new int[s.length()];
-21        Arrays.fill(dp, -1);
-22        return rec(s, 0, dp);
-23    }
-24}
+2    HashMap<String , Integer> map = new HashMap<>();
+3
+4    public int numDecodings(String s) {
+5        return answer(s);
+6    }
+7
+8    public int answer(String s){
+9        if(s.length() == 0){
+10            return 1;
+11        }
+12
+13        if(map.containsKey(s)){
+14            return map.get(s);
+15        }
+16        int ans =0;
+17        for(int cut =1 ;cut <=s.length(); cut++){
+18            String sub = s.substring(0,cut);
+19            if(isValid(sub)){
+20                ans += answer(s.substring(cut));
+21            }
+22        }
+23        map.put(s, ans);
+24        return ans;
+25
+26    }
+27    public boolean isValid(String s){
+28        if(s.length() == 0){
+29            return false;
+30        }
+31        if(s.charAt(0) == '0' || s.length()>=3 || Integer.parseInt(s)> 26){
+32            return false;
+33        }
+34        return true;
+35    }
+36}
