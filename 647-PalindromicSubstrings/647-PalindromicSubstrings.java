276358b872b6b1c99fe991a8cddb74f246a7e5cf
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-04 12:14:26 +0530

    File updated at 04/01/2026, 12:14:26

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.java
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -1,25 +1,35 @@
-// Last updated: 04/01/2026, 12:06:37
+// Last updated: 04/01/2026, 12:14:26
 1class Solution {
-2    public boolean isPal(String s, int i, int j, Boolean[][] dp) {
-3        if(i>j) return true;
-4        if(dp[i][j]!=null) return dp[i][j];
-5        boolean a = s.charAt(i) == s.charAt(j);
-6        boolean b = isPal(s, i+1, j-1, dp);
-7        return dp[i][j] = a && b;
-8    }
-9    public int countSubstrings(String s) {
-10        int n= s.length();
-11        int ans = 0;
+2    public int countSubstrings(String s) {
+3        int n = s.length();
+4        boolean[][] dp = new boolean[n][n];
+5        int count = 0;
+6
+7        // length = 1
+8        for (int i = 0; i < n; i++) {
+9            dp[i][i] = true;
+10            count++;
+11        }
 12
-13        int c= 0;
-14        Boolean[][] dp = new Boolean[n][n];
-15        for(int i=0; i<n; i++) {
-16            for(int j=i; j<n; j++) {
-17                if(isPal(s, i, j, dp)) c++;
-18            }
-19        }
-20        return c;
-21
-22        
-23    }
-24}
+13        // length >= 2
+14        for (int len = 2; len <= n; len++) {
+15            for (int i = 0; i + len - 1 < n; i++) {
+16                int j = i + len - 1;
+17
+18                if (s.charAt(i) == s.charAt(j)) {
+19                    if (len == 2) {
+20                        dp[i][j] = true;        // "aa"
+21                    } else {
+22                        dp[i][j] = dp[i + 1][j - 1];
+23                    }
+24                } else {
+25                    dp[i][j] = false;
+26                }
+27
+28                if (dp[i][j]) count++;
+29            }
+30        }
+31
+32        return count;
+33    }
+34}
