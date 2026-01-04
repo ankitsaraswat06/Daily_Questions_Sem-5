eb60d784e13b6a7938ce3e664288bde86dec8c4b
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-04 11:08:28 +0530

    File updated at 04/01/2026, 11:08:27

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,25 +1,23 @@
-// Last updated: 04/09/2025, 11:14:19
-class Solution {
-    public int rec(int i, int j, String s1, String s2, int[][] dp) {
-        if(i==s1.length() || j==s2.length()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans = 0;
-        if(s1.charAt(i)==s2.charAt(j)) {
-            ans = rec(i+1, j+1, s1, s2, dp) + 1;
-        } else {
-            int a = rec(i+1, j, s1, s2, dp);
-            int b = rec(i, j+1, s1, s2, dp);
-            ans = Math.max(a,b);
-        }
-        return dp[i][j] = ans;
-    }
-    public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
-        for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[0].length;j++) {
-                dp[i][j] = -1;
-            }
-        }
-       return rec(0, 0, text1, text2, dp); 
-    }
-}
+// Last updated: 04/01/2026, 11:08:27
+1class Solution {
+2    public int rec(String text1, String text2, int i, int j, int[][] dp) {
+3        if(i>=text1.length() || j>=text2.length()) return 0;
+4        if(dp[i][j] != -1) return dp[i][j];
+5        int a = 0;
+6        int b = 0;
+7        if(text1.charAt(i)==text2.charAt(j)) {
+8            a = rec(text1, text2, i+1, j+1, dp) + 1;
+9        } else {
+10            b = Math.max(rec(text1, text2, i+1, j, dp), rec(text1, text2, i, j+1, dp));
+11        }
+12        return dp[i][j] =  Math.max(a, b);
+13    }
+14    public int longestCommonSubsequence(String text1, String text2) {
+15        int[][] dp = new int[text1.length()][text2.length()];
+16        for(int el[]: dp) {
+17            Arrays.fill(el, -1);
+18        }
+19        return rec(text1, text2, 0, 0, dp);
+20        
+21    }
+22}
