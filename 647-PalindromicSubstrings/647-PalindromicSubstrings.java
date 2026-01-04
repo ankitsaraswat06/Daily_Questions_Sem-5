9ea973f0a92c9f15d2192d1c3a0220e534f86613
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-04 11:58:59 +0530

    File updated at 04/01/2026, 11:58:59

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.java
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -1,26 +1,23 @@
-// Last updated: 02/08/2025, 18:56:05
-class Solution {
-    public int countSubstrings(String s) {
-        int odd = 0;
-        // odd length
-        for(int axis = 0; axis<s.length(); axis++) {
-            for(int orbit = 0; axis-orbit>=0 && axis+orbit <s.length(); orbit++) {
-                if(s.charAt(axis-orbit) != s.charAt(axis+orbit)) {
-                    break;
-                }  
-                odd++; 
-            }
-        }
-        // even length
-        int even =0;
-        for(double axis = 0.5; axis<s.length(); axis++) {
-            for(double orbit = 0.5; axis-orbit>=0 && axis+orbit <s.length(); orbit++) {
-                if(s.charAt((int)(axis-orbit)) != s.charAt((int)(axis+orbit))) {
-                    break;
-                }  
-                even++; 
-            }
-        }
-        return odd+even;
-    }
-}
+// Last updated: 04/01/2026, 11:58:59
+1class Solution {
+2    public boolean isPal(String s, int i, int j) {
+3        if(i>j) return true;
+4        boolean a = s.charAt(i) == s.charAt(j);
+5        boolean b = isPal(s, i+1, j-1);
+6        return a && b;
+7    }
+8    public int countSubstrings(String s) {
+9        int n= s.length();
+10        int ans = 0;
+11
+12        int c= 0;
+13        for(int i=0; i<n; i++) {
+14            for(int j=i; j<n; j++) {
+15                if(isPal(s, i, j)) c++;
+16            }
+17        }
+18        return c;
+19
+20        
+21    }
+22}
