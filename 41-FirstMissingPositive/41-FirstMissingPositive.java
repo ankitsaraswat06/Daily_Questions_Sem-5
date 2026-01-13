062cf8dccb78352c4c3fec7d28bf34d68cc95cd3
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-13 15:45:57 +0530

    File updated at 13/01/2026, 15:45:57

--- 41-FirstMissingPositive/41-FirstMissingPositive.java
+++ 41-FirstMissingPositive/41-FirstMissingPositive.java
@@ -1,26 +1,19 @@
-// Last updated: 02/08/2025, 18:59:26
-class Solution {
-    public int firstMissingPositive(int[] nums) {    
-        int arr[] = new int[nums.length];
-        int idx= 0;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i]>0) {
-                arr[idx++] = nums[i];
-            }
-        }
-        int ans[] = Arrays.copyOfRange(arr, 0, idx);
-        Arrays.sort(ans);
-        int target = 1;
-        for(int i=0; i<ans.length; i++) {
-            System.out.print(ans[i] + " ");
-        }
-        for(int i=0; i<ans.length; i++) {
-            if(ans[i] == target) {
-                target++;
-            } else if(ans[i] > target) {
-                return target;
-            }
-        }
-        return target;
-    }
-}
+// Last updated: 13/01/2026, 15:45:57
+1class Solution {
+2    public int firstMissingPositive(int[] nums) {
+3        for(int i=0; i<nums.length; i++) {
+4            while(nums[i]>=1 && nums[i]<=nums.length && nums[i]!=nums[nums[i]-1]) {
+5                int k = nums[i]-1;
+6                int temp = nums[k];
+7                nums[k] = nums[i];
+8                nums[i] = temp;
+9            }
+10        }
+11        
+12        for(int i=0; i<nums.length; i++) {
+13            if(nums[i]!=i+1) return i+1;
+14        }
+15        return nums.length+1;
+16        
+17    }
+18}
