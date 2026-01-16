f1882a287c874c26267848b52ac1399820013c74
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-16 23:25:04 +0530

    Create 2007-FindOriginalArrayFromDoubledArray

---
+++ 2007-FindOriginalArrayFromDoubledArray/2007-FindOriginalArrayFromDoubledArray.java
@@ -0,0 +1,27 @@
+// Last updated: 16/01/2026, 23:25:04
+1class Solution {
+2    public int[] findOriginalArray(int[] changed) {
+3        int n = changed.length;
+4        if(n%2!=0) return new int[0];
+5        Arrays.sort(changed);
+6        Map<Integer, Integer> map = new HashMap<>();
+7        for(int i=0; i<n; i++) {
+8            map.put(changed[i], map.getOrDefault(changed[i], 0)+1);
+9        }
+10        List<Integer> ll = new ArrayList<>();
+11        
+12        for(int k: changed) {
+13            if(map.get(k)==0) continue;
+14            if(map.getOrDefault(k*2, 0)==0) return new int[0];
+15            ll.add(k);
+16            map.put(k, map.get(k)-1);
+17            map.put(k*2, map.get(k*2)-1);
+18        }
+19        int arr[] = new int[n/2];
+20        int idx = 0;
+21        while(ll.size()>0) {
+22            arr[idx++] = ll.remove(0);
+23        }
+24        return arr;
+25    }
+26}
