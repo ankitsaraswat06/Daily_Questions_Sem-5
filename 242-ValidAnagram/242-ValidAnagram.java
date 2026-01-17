8c1405a2689eef76c1a2a9c2e3bc6e2ff144c42c
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-17 17:08:27 +0530

    File updated at 17/01/2026, 17:08:26

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,13 +1,12 @@
-// Last updated: 28/08/2025, 10:53:14
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        int f1[] = new int[26];
-        int f2[] = new int[26];
-        for(int i=0; i<s.length(); i++) f1[s.charAt(i)-'a']++;
-        for(int i=0; i<t.length(); i++) f2[t.charAt(i)-'a']++;
-        for(int i=0; i<f1.length; i++) if(f1[i]!=f2[i]) return false;
-        return true;
-
-
-    }
-}
+// Last updated: 17/01/2026, 17:08:26
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        int f[] = new int[26];
+4        for(int i=0; i<s.length(); i++) f[s.charAt(i)-'a']++;
+5        for(int i=0; i<t.length(); i++) f[t.charAt(i)-'a']--;
+6        for(int i=0; i<f.length; i++) if(f[i]!=0) return false;
+7        return true;
+8
+9
+10    }
+11}
