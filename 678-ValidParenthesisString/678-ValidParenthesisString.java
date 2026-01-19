ebba15580e7b07c693580bf7644474b323186480
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-19 15:25:20 +0530

    File updated at 19/01/2026, 15:25:20

--- 678-ValidParenthesisString/678-ValidParenthesisString.java
+++ 678-ValidParenthesisString/678-ValidParenthesisString.java
@@ -1,23 +1,30 @@
-// Last updated: 19/01/2026, 15:05:24
+// Last updated: 19/01/2026, 15:25:20
 1class Solution {
 2    public boolean checkValidString(String s) {
-3        int minOpen = 0;
-4        int maxOpen = 0;
+3        Stack<Integer> st = new Stack<>();
+4        Stack<Integer> ast = new Stack<>();
 5        for(int i=0; i<s.length(); i++) {
-6            if(s.charAt(i)=='(') {
-7                minOpen++;
-8                maxOpen++;
-9            } else if(s.charAt(i)==')') {
-10                minOpen--;
-11                maxOpen--;
-12            } else {
-13                minOpen--;
-14                maxOpen++;
-15            }
-16            if(minOpen<0) minOpen =0;
-17            if(maxOpen<0) return false;
-18        }
-19        return minOpen==0;
-20        
-21    }
-22}
+6            char ch = s.charAt(i);
+7            if(ch=='(') st.push(i);
+8            else if(ch=='*') ast.push(i);
+9            else {
+10                if(!st.isEmpty()) {
+11                    st.pop();
+12                } else if(!ast.isEmpty()) {
+13                    ast.pop();
+14                } else {
+15                    return false;
+16                }
+17            }
+18            
+19        }
+20        while(!st.isEmpty() && !ast.isEmpty()) {
+21            if(st.peek()>ast.peek()) {
+22                return false;
+23            }
+24            st.pop();
+25            ast.pop();
+26        }
+27        return st.size()==0;
+28    }
+29}
