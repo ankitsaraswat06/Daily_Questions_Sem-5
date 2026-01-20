4941f375914c882b29b4f458ef4a4134fb3249a4
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-20 15:49:24 +0530

    File updated at 20/01/2026, 15:49:24

--- 735-AsteroidCollision/735-AsteroidCollision.java
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -1,19 +1,27 @@
-// Last updated: 02/08/2025, 18:55:53
-class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
-        for(int i=0; i<asteroids.length; i++) {
-            if(asteroids[i] > 0) st.push(asteroids[i]);
-            else {
-                while(!st.isEmpty() && st.peek()>0 && st.peek() < Math.abs(asteroids[i])) st.pop();
-                if(!st.isEmpty() && st.peek() == Math.abs(asteroids[i])) st.pop();
-                else if(st.isEmpty() || st.peek() < 0) st.push(asteroids[i]);
-            }
-        }
-        int ans[] = new int[st.size()];
-        int i=0;
-        for(int num: st) ans[i++] = num;
-        return ans;
-        
-    }
-}
+// Last updated: 20/01/2026, 15:49:24
+1class Solution {
+2    public int[] asteroidCollision(int[] arr) {
+3        Stack<Integer> st = new Stack<>();
+4        boolean flag = false;
+5        for(int i=0; i<arr.length; i++) {
+6            flag = false;
+7            while(!st.isEmpty() && arr[i] <0 && st.peek()>0) {
+8                if(st.peek()< -1 * arr[i]) {
+9                    st.pop();
+10                    continue;
+11                } else if(st.peek() == -1 * arr[i]) {
+12                    st.pop();
+13                }
+14                flag = true;
+15                break;
+16            }
+17            if(!flag) st.push(arr[i]);
+18        }
+19
+20        int[] ans = new int[st.size()];
+21        int i = 0;
+22        for(int el: st) ans[i++] = el;
+23        return ans;
+24        
+25    }
+26}
