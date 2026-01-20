105706d58228a56165d7291b53bd8cb3ac06172e
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-20 14:29:55 +0530

    File updated at 20/01/2026, 14:29:55

--- 496-NextGreaterElementI/496-NextGreaterElementI.java
+++ 496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -1,30 +1,28 @@
-// Last updated: 21/10/2025, 15:17:11
-class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n2 = nums2.length;
-        int[] nge = new int[n2];
-        Stack<Integer> st = new Stack<>();
-        for(int i=n2-1; i>=0; i--) {
-            while(!st.isEmpty() && st.peek() <= nums2[i]) {
-                st.pop();
-            }
-            if(st.isEmpty()) nge[i] = -1;
-            else nge[i] = st.peek();
-            st.push(nums2[i]);
-        }
-         
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums2.length; i++) {
-            map.put(nums2[i], nge[i]);
-        }
-        int[] ans = new int[nums1.length];
-
-        for(int i=0; i<nums1.length; i++) {
-            ans[i] = map.get(nums1[i]);
-        }
-
-
-        return ans;
-        
-    }
-}
+// Last updated: 20/01/2026, 14:29:55
+1class Solution {
+2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+3        int n2 = nums2.length;
+4        int[] nge = new int[n2];
+5        Stack<Integer> st = new Stack<>();
+6        for(int i=n2-1; i>=0; i--) {
+7            while(!st.isEmpty() && st.peek() <= nums2[i]) {
+8                st.pop();
+9            }
+10            if(st.isEmpty()) nge[i] = -1;
+11            else nge[i] = st.peek();
+12            st.push(nums2[i]);
+13        }
+14         
+15        Map<Integer, Integer> map = new HashMap<>();
+16        for (int i = 0; i < nums2.length; i++) {
+17            map.put(nums2[i], nge[i]);
+18        }
+19        int[] ans = new int[nums1.length];
+20
+21        for(int i=0; i<nums1.length; i++) {
+22            ans[i] = map.get(nums1[i]);
+23        }
+24        return ans;
+25        
+26    }
+27}
