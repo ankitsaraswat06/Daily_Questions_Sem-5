96db4e90fe0f07522eb1af469a11fe8bfd066e71
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-20 14:14:39 +0530

    File updated at 20/01/2026, 14:14:38

--- 682-BaseballGame/682-BaseballGame.java
+++ 682-BaseballGame/682-BaseballGame.java
@@ -1,29 +1,29 @@
-// Last updated: 02/08/2025, 18:56:03
-class Solution {
-    public int calPoints(String[] operations) {
-        Stack<Integer> st = new Stack<>();
-        for(int i=0; i<operations.length; i++) {
-            if(operations[i].equals("C")) {
-                st.pop();
-            }else if(operations[i].equals("D")) {
-                st.push(st.peek()*2);
-            } else if(operations[i].equals("+")) {
-                int x2 = st.pop();
-                int x1 = st.pop();
-                int x3 = x1+x2;
-                st.push(x1);
-                st.push(x2);
-                st.push(x3);  
-            } else {
-                int k = Integer.parseInt(operations[i]);
-                st.push(k);
-            }
-        }
-        int sum = 0;
-        while(st.size() > 0) {
-            sum+=st.pop();
-        }
-        return sum;
-        
-    }
-}
+// Last updated: 20/01/2026, 14:14:38
+1class Solution {
+2    public int calPoints(String[] operations) {
+3        Stack<Integer> st = new Stack<>();
+4        for(int i=0; i<operations.length; i++) {
+5            if(operations[i].equals("C")) {
+6                st.pop();
+7            }else if(operations[i].equals("D")) {
+8                st.push(st.peek()*2);
+9            } else if(operations[i].equals("+")) {
+10                int x2 = st.pop();
+11                int x1 = st.pop();
+12                int x3 = x1+x2;
+13                st.push(x1);
+14                st.push(x2);
+15                st.push(x3);  
+16            } else {
+17                int k = Integer.parseInt(operations[i]);
+18                st.push(k);
+19            }
+20        }
+21        int sum = 0;
+22        while(st.size() > 0) {
+23            sum+=st.pop();
+24        }
+25        return sum;
+26        
+27    }
+28}
