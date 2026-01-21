d3ab035c2aca81d639a265ba6fa6a3c9be59cb43
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-21 11:32:26 +0530

    File updated at 21/01/2026, 11:32:26

--- 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
+++ 2683-NeighboringBitwiseXOR/2683-NeighboringBitwiseXOR.java
@@ -1,9 +1,10 @@
-// Last updated: 21/01/2026, 11:31:55
+// Last updated: 21/01/2026, 11:32:26
 1class Solution {
 2    public boolean doesValidArrayExist(int[] derived) {
 3        int xor = 0;
 4        for(int el: derived) xor^=el;
 5        return xor ==0;
-6        
-7    }
-8}
+6        // [a^b, b^c, c^a] --> derived array and [a, b, c] is original array;
+7        
+8    }
+9}
