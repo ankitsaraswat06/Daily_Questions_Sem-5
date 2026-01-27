d372e2101e086c527fa2379ec1bac785f6ad8cfa
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-27 15:10:17 +0530

    File updated at 27/01/2026, 15:10:17

--- 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.java
+++ 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.java
@@ -1,17 +1,13 @@
-// Last updated: 27/01/2026, 14:58:28
+// Last updated: 27/01/2026, 15:10:17
 1class Solution {
 2    public int findTheWinner(int n, int k) {
-3        Queue<Integer> q = new ArrayDeque<>();
-4        for(int i=1; i<=n; i++) q.offer(i);
-5        while(q.size()>1) {
-6            int m = k;
-7            while(m-->1) {
-8               q.offer(q.poll());
-9            }
-10            q.poll();
-11            
-12        }
-13        return q.poll();
-14        
-15    }
-16}
+3        int ans = 0;
+4        int i=1;
+5        while(i<=n) {
+6            ans = (ans+k)%i;
+7            i++;
+8        }
+9        return ans+1;
+10        
+11    }
+12}
