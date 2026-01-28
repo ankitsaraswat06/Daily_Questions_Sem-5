cb0b25ae2e0efe190832f58e6fa830e60549591d
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-28 12:16:45 +0530

    File updated at 28/01/2026, 12:16:44

--- 394-DecodeString/394-DecodeString.java
+++ 394-DecodeString/394-DecodeString.java
@@ -1,34 +1,38 @@
-// Last updated: 02/08/2025, 18:56:54
-class Solution {
-    public String decodeString(String s) {
-        Stack<String> st = new Stack<>();
-        int num = 0;
-
-        for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if(Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
-            } else if(ch == '[') {
-                st.push(String.valueOf(num));
-                st.push("[");
-                num = 0;
-            } else if(ch == ']') {
-                StringBuilder sb = new StringBuilder();
-                while(!st.peek().equals("[")) {
-                    sb.insert(0, st.pop());
-                }
-                st.pop();
-                int count = Integer.parseInt(st.pop());
-                String repeated = sb.toString().repeat(count);
-                st.push(repeated);
-            } else {
-                st.push(String.valueOf(ch)); 
-            }
-        }
-        StringBuilder result = new StringBuilder();
-        for(String str : st) result.append(str);
-
-        return result.toString();
-    }
-}
+// Last updated: 28/01/2026, 12:16:44
+1class Solution {
+2    public String decodeString(String s) {
+3        Stack<String> st = new Stack<>();
+4        // String ans ="";
+5        int num = 0;
+6        for(int i=0; i<s.length(); i++) {
+7            char ch = s.charAt(i);
+8            if(Character.isDigit(ch)) {
+9                int j = ch - '0';
+10                num = num*10 + j;
+11            } else if(ch=='[') {
+12                st.push(String.valueOf(num));
+13                st.push(String.valueOf(ch));
+14                num = 0;
+15            } else if(ch==']') {
+16                StringBuilder sb= new StringBuilder();
+17                while(!st.peek().equals("[")) {
+18                    sb.insert(0, st.pop());
+19                }
+20                st.pop();
+21                int c = Integer.parseInt(st.pop());
+22                String repeated = sb.toString().repeat(c);
+23                st.push(repeated);
+24            } else {
+25                st.push(String.valueOf(ch));
+26            }
+27
+28            // System.out.println(st);
+29        }
+30
+31        StringBuilder ans = new StringBuilder();
+32        while(!st.isEmpty()) {
+33            ans.insert(0, st.pop());
+34        }
+35        return ans.toString();
+36    }
+37}
