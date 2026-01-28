8722603768960d67fb198bb5eee2c34dbe959fba
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-28 13:06:55 +0530

    File updated at 28/01/2026, 13:06:55

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,12 +1,12 @@
-// Last updated: 01/10/2025, 14:14:54
-class Solution {
-    public boolean canJump(int[] nums) {
-        int n= nums.length;
-        int maxReach = 0;
-        for(int i=0; i<n; i++) {
-            if(i>maxReach) return false;
-            maxReach = Math.max(maxReach, i+nums[i]);
-        }  
-        return true;
-    }
-}
+// Last updated: 28/01/2026, 13:06:55
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int n= nums.length;
+4        int maxReach = 0;
+5        for(int i=0; i<n; i++) {
+6            if(i>maxReach) return false;
+7            maxReach = Math.max(maxReach, i+nums[i]);
+8        }  
+9        return true;
+10    }
+11}
