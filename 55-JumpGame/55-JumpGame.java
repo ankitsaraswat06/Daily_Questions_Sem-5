ab67361dffea0e4cb3932226e6c3413ddddfe954
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-28 13:14:09 +0530

    File updated at 28/01/2026, 13:14:08

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,12 +1,20 @@
-// Last updated: 28/01/2026, 13:07:20
+// Last updated: 28/01/2026, 13:14:08
 1class Solution {
-2    public boolean canJump(int[] nums) {
-3        int maxReach = 0;
-4        for(int i=0; i<nums.length; i++) {
-5            if(i>maxReach) return false;
-6            maxReach = Math.max(maxReach, nums[i]+i);
-7        }
-8        return true;
-9        
-10    }
-11}
+2    public boolean rec(int[] nums, int idx, Boolean[] dp) {
+3        if(idx==nums.length-1) return true;
+4        if(idx>=nums.length) return false;
+5        if(dp[idx]!=null) return dp[idx];
+6        
+7        int jump = nums[idx];
+8        boolean ans = false;
+9        for(int i=1; i<=jump; i++) {
+10            ans = ans || rec(nums, idx+i, dp);
+11        }
+12        return dp[idx] = ans;
+13    }
+14    public boolean canJump(int[] nums) {
+15        Boolean[] dp = new Boolean[nums.length];
+16        return rec(nums, 0, dp);
+17        
+18    }
+19}
