1185581406908ad48f09c203f5b38c72d9ae43df
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-28 13:30:32 +0530

    File updated at 28/01/2026, 13:30:32

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,19 +1,22 @@
-// Last updated: 01/10/2025, 14:38:42
-class Solution {
-    public int jump(int[] nums) {
-        int near = 0, far = 0, jump = 0;
-        int n = nums.length;
-        int farthest = 0;
-        while(far<n-1) {
-            
-            for(int i=near; i<=far; i++) {
-                farthest = Math.max(farthest, i + nums[i]);
-            }
-            near = far+1;
-            far = farthest;
-            jump++;
-        }
-        return jump;
-        
-    }
-}
+// Last updated: 28/01/2026, 13:30:32
+1class Solution {
+2    public int rec(int[] nums, int idx, int[] dp) {
+3        if(idx>=nums.length-1) return 0;
+4        if(dp[idx]!=-1) return dp[idx];
+5        if(nums[idx]==0) return Integer.MAX_VALUE;
+6        int ans = Integer.MAX_VALUE;
+7        for(int i=1; i<=nums[idx]; i++) {
+8            int a = rec(nums, idx+i, dp);
+9            if(a!=Integer.MAX_VALUE) {
+10                ans = Math.min(a+1, ans);
+11            }
+12        }
+13        return dp[idx]= ans;
+14    }
+15    public int jump(int[] nums) {
+16        int[] dp = new int[nums.length];
+17        Arrays.fill(dp, -1);
+18        return rec(nums, 0, dp);
+19        
+20    }
+21}
