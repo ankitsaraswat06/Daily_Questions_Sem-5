b76b9b0ee75031895d083642a1b7ee7dcc165e42
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-02-02 14:36:44 +0530

    File updated at 02/02/2026, 14:36:44

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.java
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.java
@@ -1,45 +1,42 @@
-// Last updated: 31/10/2025, 12:57:53
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode temp = head;
-        while(temp!=null) {
-            ListNode q  = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = q;
-        }
-        return prev;
-    }
-    public boolean isPalindrome(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast.next!=null && fast.next.next!=null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode newHead = reverse(slow.next);
-        ListNode first = head;
-        ListNode second = newHead;
-
-        while(second!=null) {
-            if(first.val!=second.val) {
-                return false;
-            }
-            first = first.next;
-            second = second.next;
-        }
-        return true;
-
-    }
-}
+// Last updated: 02/02/2026, 14:36:44
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode reverse(ListNode head) {
+13        ListNode prev = null;
+14        ListNode curr = head;
+15        while(curr!=null) {
+16            ListNode next = curr.next;
+17            curr.next = prev;
+18            prev = curr;
+19            curr = next;
+20        }
+21        return prev;
+22    }
+23    public boolean isPalindrome(ListNode head) {
+24        ListNode slow = head;
+25        ListNode fast = head;
+26        while(fast.next!=null && fast.next.next!=null) {
+27            slow = slow.next;
+28            fast = fast.next.next;
+29        }
+30        ListNode newHead = reverse(slow.next);
+31        ListNode first = head;
+32        ListNode second = newHead;
+33        while(second!=null) {
+34            if(first.val!=second.val) return false;
+35            first = first.next;
+36            second = second.next;
+37        }
+38        return true;
+39        
+40    }
+41}
