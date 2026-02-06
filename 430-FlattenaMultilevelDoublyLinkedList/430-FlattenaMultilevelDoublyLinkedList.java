737b80257a48624065cc4b8b56783c47b8cae8f6
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-02-06 12:41:18 +0530

    File updated at 06/02/2026, 12:41:18

--- 430-FlattenaMultilevelDoublyLinkedList/430-FlattenaMultilevelDoublyLinkedList.java
+++ 430-FlattenaMultilevelDoublyLinkedList/430-FlattenaMultilevelDoublyLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 06/02/2026, 12:39:24
+// Last updated: 06/02/2026, 12:41:18
 1/*
 2// Definition for a Node.
 3class Node {
@@ -16,19 +16,20 @@
 15        while(curr!=null) {
 16            if(curr.child!=null) {
 17                if(curr.next!=null) st.push(curr.next);
-18                curr.next= curr.child;
-19                Node nn = curr.child;
-20                curr.child = null;
-21                nn.prev = curr;
-22            }
-23            if(curr.next==null && !st.isEmpty()) {
-24                Node nn = st.pop();
-25                curr.next =nn;
-26                nn.prev = curr;   
-27            }
-28            curr = curr.next;
-29        }
-30        return head;
-31        
-32    }
-33}
+18                Node nn = curr.child;
+19                curr.next= curr.child;
+20                
+21                curr.child = null;
+22                nn.prev = curr;
+23            }
+24            if(curr.next==null && !st.isEmpty()) {
+25                Node nn = st.pop();
+26                curr.next =nn;
+27                nn.prev = curr;   
+28            }
+29            curr = curr.next;
+30        }
+31        return head;
+32        
+33    }
+34}
