74c11b7cea7f70376795c0387ed478d401d5b31f
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-02-19 14:53:25 +0530

    File updated at 19/02/2026, 14:53:24

--- 696-CountBinarySubstrings/696-CountBinarySubstrings.java
+++ 696-CountBinarySubstrings/696-CountBinarySubstrings.java
@@ -1,19 +1,19 @@
-// Last updated: 02/08/2025, 18:55:59
-class Solution {
-    public int countBinarySubstrings(String s) {
-        int currentCount = 1;
-        int previousCount = 0;
-        int ans = 0;
-        for(int i=1; i<s.length(); i++) {
-            if(s.charAt(i) == s.charAt(i-1)) {
-                currentCount++;
-            } else {
-                ans+=Math.min(currentCount, previousCount);
-                previousCount = currentCount;
-                currentCount = 1;
-            }
-        }
-        ans+=Math.min(currentCount, previousCount);
-        return ans;
-    }
-}
+// Last updated: 19/02/2026, 14:53:24
+1class Solution {
+2    public int countBinarySubstrings(String s) {
+3        int currentCount = 1;
+4        int previousCount = 0;
+5        int ans = 0;
+6        for(int i=1; i<s.length(); i++) {
+7            if(s.charAt(i) == s.charAt(i-1)) {
+8                currentCount++;
+9            } else {
+10                ans+=Math.min(currentCount, previousCount);
+11                previousCount = currentCount;
+12                currentCount = 1;
+13            }
+14        }
+15        ans+=Math.min(currentCount, previousCount);
+16        return ans;
+17    }
+18}
