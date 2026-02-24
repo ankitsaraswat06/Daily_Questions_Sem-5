d47138a5319d68663a54ed25b94438e2414615b0
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-02-24 15:22:58 +0530

    File updated at 24/02/2026, 15:22:58

--- 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.java
+++ 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.java
@@ -1,30 +1,28 @@
-// Last updated: 31/12/2025, 10:36:07
+// Last updated: 24/02/2026, 15:22:58
 1class Solution {
 2    public boolean validPath(int n, int[][] edges, int source, int destination) {
-3        Map<Integer, List<Integer>> graph = new HashMap<>();
+3        Map<Integer, List<Integer>> map = new HashMap<>();
 4        for(int i=0; i<n; i++) {
-5            graph.put(i, new ArrayList<>());
+5            map.put(i, new ArrayList<>());
 6        }
 7        for(int i=0; i<edges.length; i++) {
-8            int a = edges[i][0];
-9            int b= edges[i][1];
-10            graph.get(a).add(b);
-11            graph.get(b).add(a);
+8            int a= edges[i][0];
+9            int b = edges[i][1];
+10            map.get(a).add(b);
+11            map.get(b).add(a);
 12        }
-13
+13        HashSet<Integer> vis = new HashSet<>();
 14        Stack<Integer> st = new Stack<>();
-15        HashSet<Integer> visited = new HashSet<>();
-16        st.push(source);
-17        while(!st.isEmpty()) {
-18            int rv = st.pop();
-19            if(visited.contains(rv)) continue;
-20            visited.add(rv);
-21            if(rv==destination) return true;
-22            for(int nbr: graph.get(rv)) {
-23                if(!visited.contains(nbr)) st.push(nbr);
-24            }
-25        }
-26        return false;
-27        
-28    }
-29}
+15        st.push(source);
+16        while(!st.isEmpty()) {
+17            int rv = st.pop();
+18            if(vis.contains(rv)) continue;
+19            vis.add(rv);
+20            if(rv==destination) return true;
+21            for(int nbr: map.get(rv)) {
+22                if(!vis.contains(nbr)) st.push(nbr);
+23            }
+24        }
+25        return false;
+26    }
+27}
