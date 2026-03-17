09b3a035723522e1eba652bdcf500ae7352adbb8
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-03-17 22:08:07 +0530

    File updated at 17/03/2026, 22:08:06

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,18 +1,19 @@
-// Last updated: 26/09/2025, 11:58:45
-class Solution {
-    public int majorityElement(int[] nums) {
-        int e = nums[0];
-        int v = 1;
-        for(int i=1; i<nums.length; i++) {
-            if(nums[i]==e) v++;
-            else {
-                v--;
-                if(v==0) {
-                    e = nums[i];
-                    v = 1;
-                }
-            }
-        }
-        return e;
-    }
-}
+// Last updated: 17/03/2026, 22:08:06
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        int v = 1;
+4        int cand = nums[0];
+5        for(int i=1; i<nums.length; i++) {
+6            if(nums[i]==cand) v++;
+7            else {
+8                v--;
+9                if(v==0) {
+10                    cand = nums[i];
+11                    v = 1;
+12                }
+13            }
+14        }
+15        return cand;
+16    }
+17
+18}
