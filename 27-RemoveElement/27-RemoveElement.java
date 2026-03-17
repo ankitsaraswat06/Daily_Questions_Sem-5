abac4dc272ba4efe3a7bea1de20ffbf7fb7d774d
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-03-17 22:03:10 +0530

    File updated at 17/03/2026, 22:03:09

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,14 +1,14 @@
-// Last updated: 02/08/2025, 18:59:41
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        int idx = 0;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i]!=val) {
-                nums[idx++] = nums[i];
-            }
-        }
-
-        return idx;
-        
-    }
-}
+// Last updated: 17/03/2026, 22:03:09
+1class Solution {
+2    public int removeElement(int[] nums, int val) {
+3        
+4        int idx = 0;
+5        for(int i=0; i<nums.length; i++) {
+6            if(nums[i]!=val) {
+7                nums[idx++] = nums[i];
+8            }
+9        }
+10        return idx;
+11        
+12    }
+13}
