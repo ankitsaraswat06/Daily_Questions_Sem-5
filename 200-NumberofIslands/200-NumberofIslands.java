8d2e84437704d00a3a4466dbea9df829d4d94d20
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-03-21 11:00:17 +0530

    File updated at 21/03/2026, 11:00:17

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,30 +1,24 @@
-// Last updated: 31/12/2025, 23:37:25
+// Last updated: 21/03/2026, 11:00:17
 1class Solution {
-2    public void dfs(char[][] grid, int i, int j) {
-3        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]!='1') {
-4            return;
-5        }
-6        grid[i][j] = 'V';
-7        dfs(grid, i-1, j);
-8        dfs(grid, i+1, j);
-9        dfs(grid, i, j-1);
-10        dfs(grid, i, j+1);
-11
-12        
-13    }
-14    public int numIslands(char[][] grid) {
-15        int rows = grid.length;
-16        int cols = grid[0].length;
-17
-18        int islands = 0;
-19        for(int i=0; i<rows; i++) {
-20            for(int j=0; j<cols; j++) {
-21                if(grid[i][j]=='1') {
-22                    dfs(grid, i, j);
-23                    islands++;
-24                }
-25            }
-26        }
-27        return islands;
-28    }
-29}
+2    public void rec(int i, int j, char[][] grid) {
+3        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]!='1') return;
+4        grid[i][j] = 'V';
+5        rec(i+1, j, grid);
+6        rec(i-1, j, grid);
+7        rec(i, j+1, grid);
+8        rec(i, j-1, grid);
+9    }
+10    public int numIslands(char[][] grid) {
+11        int island = 0;
+12        for(int i=0; i<grid.length; i++) {
+13            for(int j=0; j<grid[0].length; j++) {
+14                if(grid[i][j]=='1') {
+15                    rec(i, j, grid);
+16                    island++;
+17                }
+18            }
+19        }
+20        return island;
+21        
+22    }
+23}
